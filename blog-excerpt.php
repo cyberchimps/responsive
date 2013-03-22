9214376a659af12b29067e480bc6037f64f7280c
Simon S <simon@cyberchimps.com>
2013-03-22 15:23:35 +0530

    Next/previous link fix.

--- blog-excerpt.php
+++ blog-excerpt.php
@@ -28,7 +28,7 @@ get_header();
 			
 	<?php 
 	global $wp_query, $paged;
-	$paged = ( get_query_var( 'page' ) ? get_query_var( 'page' ) : 1 );
+	$paged = ( get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1 );
 	$blog_query = new WP_Query( array( 'post_type' => 'post', 'paged' => $paged ) );
 	$temp_query = $wp_query;
 	$wp_query = null;
