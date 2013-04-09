6d75da66ba619ee81f82f9430777dea1a4f08242
marutim <marutim@mindfiresolutions.com>
2013-04-09 17:35:25 +0530

    Default front page translation fix.

--- front-page.php
+++ front-page.php
@@ -48,11 +48,34 @@ if ( 'posts' == get_option( 'show_on_front' ) && $responsive_options['front_page
 	
 		<div class="grid col-460">
 
-			<h1 class="featured-title"><?php echo $responsive_options['home_headline']; ?></h1>
+			<h1 class="featured-title">
+				<?php
+				if ( isset( $responsive_options['home_headline'] ) && !empty( $responsive_options['home_headline'] ) )
+					echo $responsive_options['home_headline'];
+				else
+					_e( 'Hello, World!', 'responsive' );
+				?>
+			</h1>
 			
-			<h2 class="featured-subtitle"><?php echo $responsive_options['home_subheadline']; ?></h2>
+			<h2 class="featured-subtitle">
+				<?php
+				if ( isset( $responsive_options['home_subheadline'] ) && !empty( $responsive_options['home_subheadline'] ) )
+					echo $responsive_options['home_subheadline'];
+				else
+					_e( 'Your H2 subheadline here', 'responsive' );
+				?>
+			</h2>
 			
-			<p><?php echo do_shortcode( $responsive_options['home_content_area'] ); ?></p>
+			<p>
+				<?php
+				if ( isset( $responsive_options['home_content_area'] ) && !empty( $responsive_options['home_content_area'] ) )
+					echo do_shortcode( $responsive_options['home_content_area'] );
+				else
+					_e( 'Your title, subtitle and this very content is editable from Theme Option.
+					   Call to Action button and its destination link as well.
+					   Image on your right can be an image or even YouTube video if you like.','responsive' );
+				?>
+			</p>
 			
 			<?php if ($responsive_options['cta_button'] == 0): ?>  
    
