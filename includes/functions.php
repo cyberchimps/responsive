743077d9fa9e2a8bd1602572e1486383c603f218
Ulrich Pogson <grapplerulrich@gmail.com>
2013-04-13 15:39:38 +0200

    Add simple check for better compatibility

--- includes/functions.php
+++ includes/functions.php
@@ -367,7 +367,8 @@ add_filter('gallery_style', 'responsive_remove_gallery_css');
  */
 function responsive_remove_recent_comments_style() {
 	global $wp_widget_factory;
-	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+	if ( isset($wp_widget_factory->widgets['WP_Widget_Recent_Comments']) )
+		remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
 }
 add_action( 'widgets_init', 'responsive_remove_recent_comments_style' );
 
