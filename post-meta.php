b5466739bea433b848fd92121a3e02c3851c6123
Hugo Saner <hugo@sanerdesign.com>
2013-04-15 09:30:47 +0100

    Single Post made header not a link

--- post-meta.php
+++ post-meta.php
@@ -1,14 +1,15 @@
 <?php
 
 // Exit if accessed directly
-if ( !defined('ABSPATH')) exit;
+if ( !defined( 'ABSPATH' ) )
+	exit ;
 
 /**
  * Post Meta-Data Template-Part File
  *
  * @file           post-meta.php
- * @package        Responsive 
- * @author         Emil Uzelac 
+ * @package        Responsive
+ * @author         Emil Uzelac
  * @copyright      2003 - 2013 ThemeID
  * @license        license.txt
  * @version        Release: 1.1.0
@@ -18,7 +19,11 @@ if ( !defined('ABSPATH')) exit;
  */
 ?>
 
-<h1 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'responsive'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h1>
+<?php if ( is_single() ): ?>
+				<h1 class="post-title"><?php the_title( ); ?></h1>
+<?php else: ?>
+				<h1 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf( __( 'Permanent Link to %s', 'responsive' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title( ); ?></a></h1>
+<?php endif; ?>
 
 <div class="post-meta">
 <?php responsive_post_meta_data(); ?>
@@ -26,7 +31,7 @@ if ( !defined('ABSPATH')) exit;
 	<?php if ( comments_open() ) : ?>
 		<span class="comments-link">
 		<span class="mdash">&mdash;</span>
-	<?php comments_popup_link(__('No Comments &darr;', 'responsive'), __('1 Comment &darr;', 'responsive'), __('% Comments &darr;', 'responsive')); ?>
+	<?php comments_popup_link( __( 'No Comments &darr;', 'responsive' ), __( '1 Comment &darr;', 'responsive' ), __( '% Comments &darr;', 'responsive' ) ); ?>
 		</span>
 	<?php endif; ?> 
 </div><!-- end of .post-meta -->
