6b12b6f66cf221684823777353860865cbf47a52
Ulrich Pogson <grapplerulrich@gmail.com>
2013-04-22 00:04:28 +0200

    Fix issue with All in One SEO Pack

--- includes/functions.php
+++ includes/functions.php
@@ -260,27 +260,31 @@ add_filter('wp_page_menu', 'responsive_wp_page_menu');
  * @see http://codex.wordpress.org/Plugin_API/Filter_Reference/wp_title
  *
  */
-function responsive_wp_title( $title, $sep ) {
-	global $paged, $page;
+if (!function_exists('responsive_post_meta_data') && ! defined( 'AIOSEOP_VERSION' ) ) :
 
-	if ( is_feed() )
+	function responsive_wp_title( $title, $sep ) {
+		global $paged, $page;
+
+		if ( is_feed() )
+			return $title;
+
+		// Add the site name.
+		$title .= get_bloginfo( 'name' );
+
+		// Add the site description for the home/front page.
+		$site_description = get_bloginfo( 'description', 'display' );
+		if ( $site_description && ( is_home() || is_front_page() ) )
+			$title = "$title $sep $site_description";
+
+		// Add a page number if necessary.
+		if ( $paged >= 2 || $page >= 2 )
+			$title = "$title $sep " . sprintf( __( 'Page %s', 'responsive' ), max( $paged, $page ) );
+
 		return $title;
+	}
+	add_filter( 'wp_title', 'responsive_wp_title', 10, 2 );
 
-	// Add the site name.
-	$title .= get_bloginfo( 'name' );
-
-	// Add the site description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		$title = "$title $sep $site_description";
-
-	// Add a page number if necessary.
-	if ( $paged >= 2 || $page >= 2 )
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'responsive' ), max( $paged, $page ) );
-
-	return $title;
-}
-add_filter( 'wp_title', 'responsive_wp_title', 10, 2 );
+endif;
 
 /**
  * Filter 'get_comments_number'
