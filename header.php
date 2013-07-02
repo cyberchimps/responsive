606e8ab16f75a9939c30be89c6087b74672a468a
Ulrich Pogson <grapplerulrich@gmail.com>
2013-07-02 07:22:22 +0200

    Allow pinch to zoom
    http://a11yproject.com/posts/never-use-maximum-scale/

--- header.php
+++ header.php
@@ -27,7 +27,7 @@ if ( !defined('ABSPATH')) exit;
 <head>
 
 <meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <title><?php wp_title('&#124;', true, 'right'); ?></title>
 
