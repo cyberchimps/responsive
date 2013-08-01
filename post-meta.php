2b5b20542989c0892726174e5b5c0c8696be984c
Simon S <simon@cyberchimps.com>
2013-08-01 18:18:22 +0530

    Changed post title tag of blog post from h1 to h2

--- post-meta.php
+++ post-meta.php
@@ -22,7 +22,7 @@ if ( !defined( 'ABSPATH' ) )
 <?php if ( is_single() ): ?>
 				<h1 class="entry-title post-title"><?php the_title( ); ?></h1>
 <?php else: ?>
-				<h1 class="entry-title post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title( ); ?></a></h1>
+				<h2 class="entry-title post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title( ); ?></a></h2>
 <?php endif; ?>
 
 <div class="post-meta">
