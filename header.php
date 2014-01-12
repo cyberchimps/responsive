07948103bbf30b32cf0be5a20d825cab73465d2a
Ulrich Pogson <grapplerulrich@gmail.com>
2014-01-12 19:56:19 +0100

    Remove old code from header

--- header.php
+++ header.php
@@ -70,17 +70,7 @@ if( !defined( 'ABSPATH' ) ) {
 		<?php if( get_header_image() != '' ) : ?>
 
 			<div id="logo">
-				<a href="<?php echo home_url( '/' ); ?>"><img src="<?php header_image(); ?>" width="<?php if( function_exists( 'get_custom_header' ) ) {
-						echo get_custom_header()->width;
-					}
-					else {
-						echo HEADER_IMAGE_WIDTH;
-					} ?>" height="<?php if( function_exists( 'get_custom_header' ) ) {
-						echo get_custom_header()->height;
-					}
-					else {
-						echo HEADER_IMAGE_HEIGHT;
-					} ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
+				<a href="<?php echo home_url( '/' ); ?>"><img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
 			</div><!-- end of #logo -->
 
 		<?php endif; // header image was removed ?>
