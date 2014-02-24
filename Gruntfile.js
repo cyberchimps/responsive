1a81cfbc8be03ac7b935af31ee87c696b6ea46d5
Ulrich Pogson <grapplerulrich@gmail.com>
2014-02-24 21:07:50 +0100

    Update Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,48 +6,6 @@ require('load-grunt-tasks')(grunt);
 // Project configuration.
 grunt.initConfig({
 	pkg: grunt.file.readJSON('package.json'),
-	uglify: {
-		options: {
-			compress: {
-				global_defs: {
-					"EO_SCRIPT_DEBUG": false
-				},
-				dead_code: true
-				},
-			banner: '/*! <%= pkg.name %> <%= pkg.version %> */\n'
-		},
-		build: {
-				files: [
-					{
-						expand: true,     // Enable dynamic expansion.
-						src: ['js/*.js', '!js/*.min.js' ], // Actual pattern(s) to match.
-						ext: '.min.js',   // Dest filepaths will have this extension.
-					},
-			]
-		}
-	},
-
-	jshint: {
-		options: {
-			reporter: require('jshint-stylish'),
-			globals: {
-				"EO_SCRIPT_DEBUG": false,
-			},
-			'-W020': true, //Read only - error when assigning EO_SCRIPT_DEBUG a value.
-		},
-		all: [ 'js/*.js', '!js/*.min.js' ]
-	},
-
-	cssmin: {
-		add_banner: {
-			options: {
-				banner: '/* Theme Name: Responsive Author: CyberChimps.com Version: 1.9.4.9 Text Domain: responsive */'
-			},
-			files: {
-				'core/css/style.min.css': ['core/css/style.css', 'core/css/responsive.css']
-			}
-		}
-	},
 
 	//Compress build directory into <name>.zip and <name>-<version>.zip
 	compress: {
@@ -65,7 +23,6 @@ grunt.initConfig({
 
 	// Clean up build directory
 	clean: {
-
 		main: ['build/<%= pkg.name %>']
 	},
 
@@ -117,15 +74,15 @@ grunt.initConfig({
 				'_n_noop:1,2',
 				'_nx_noop:1,2,3c'
 			],
-			},
-		files:{
-		src:  [
-			'**/*.php',
-			'!node_modules/**',
-			'!build/**',
-			'!**/*~',
-		],
-	expand: true,
+		},
+		files: {
+			src: [
+				'**/*.php',
+				'!node_modules/**',
+				'!build/**',
+				'!**/*~',
+			],
+			expand: true
 		}
 	},
 
@@ -134,29 +91,29 @@ grunt.initConfig({
 			text_domain: '<%= pkg.name %>',
 			correct_domain: true,
 			keywords: [
-			'__:1,2d',
-			'_e:1,2d',
-			'_x:1,2c,3d',
-			'esc_html__:1,2d',
-			'esc_html_e:1,2d',
-			'esc_html_x:1,2c,3d',
-			'esc_attr__:1,2d',
-			'esc_attr_e:1,2d',
-			'esc_attr_x:1,2c,3d',
-			'_ex:1,2c,3d',
-			'_n:1,2,4d',
-			'_nx:1,2,4c,5d',
-			'_n_noop:1,2,3d',
-			'_nx_noop:1,2,3c,4d'
+				'__:1,2d',
+				'_e:1,2d',
+				'_x:1,2c,3d',
+				'esc_html__:1,2d',
+				'esc_html_e:1,2d',
+				'esc_html_x:1,2c,3d',
+				'esc_attr__:1,2d',
+				'esc_attr_e:1,2d',
+				'esc_attr_x:1,2c,3d',
+				'_ex:1,2c,3d',
+				'_n:1,2,4d',
+				'_nx:1,2,4c,5d',
+				'_n_noop:1,2,3d',
+				'_nx_noop:1,2,3c,4d'
 			]
 		},
 		files: {
 			src:  [
-			'**/*.php',
-			'!**/class-tgm-plugin-activation.php',
-			'!node_modules/**',
-			'!build/**',
-			'!**/*~'
+				'**/*.php',
+				'!**/class-tgm-plugin-activation.php',
+				'!node_modules/**',
+				'!build/**',
+				'!**/*~'
 			],
 			expand: true
 		}
@@ -164,12 +121,7 @@ grunt.initConfig({
 
 });
 
-
 // Default task(s).
-grunt.registerTask( 'default', ['jshint', 'uglify', 'cssmin'] );
-
-grunt.registerTask( 'test', [ 'jshint', 'checktextdomain' ] );
-
-grunt.registerTask( 'build', [ 'test', 'newer:uglify', 'cssmin', 'newer:po2mo', 'clean', 'copy', 'compress' ] );
+grunt.registerTask( 'default', [ 'checktextdomain', 'po2mo', 'clean', 'copy', 'compress' ] );
 
 };
