9eacb20ebad77a35f1dedddb9ffab75c131f4a60
Ulrich Pogson <grapplerulrich@gmail.com>
2014-03-14 10:13:21 +0100

    Update theme options location

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ require( $template_directory . '/core/includes/functions.php' );
 require( $template_directory . '/core/includes/functions-update.php' );
 require( $template_directory . '/core/includes/functions-sidebar.php' );
 require( $template_directory . '/core/includes/functions-install.php' );
-require( $template_directory . '/core/includes/theme-options.php' );
+require( $template_directory . '/core/includes/theme-options/theme-options.php' );
 require( $template_directory . '/core/includes/post-custom-meta.php' );
 require( $template_directory . '/core/includes/tha-theme-hooks.php' );
 require( $template_directory . '/core/includes/hooks.php' );
