aa5c59fd484ac659181a9c6451241898fde1f547
Ujjawal <ujjawal.agrawal@hbwsl.com>
2015-05-20 15:09:11 +0530

    Changes for Theme Customizer as per wordpress.org guidelines

--- template-parts/featured-area.php
+++ template-parts/featured-area.php
@@ -9,8 +9,19 @@ $responsive_options = responsive_get_options();
 //test for first install no database
 $db = get_option( 'responsive_theme_options' );
 //test if all options are empty so we can display default text if they are
-$empty     = ( empty( $responsive_options['home_headline'] ) && empty( $responsive_options['home_subheadline'] ) && empty( $responsive_options['home_content_area'] ) ) ? false : true;
-$emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
+
+$home_headline = get_theme_mod('home_headline');
+
+$home_subheadline = get_theme_mod('home_subheadline');
+
+$home_content_area = get_theme_mod('home_content_area');
+
+$cta_text = get_theme_mod('cta_text');
+
+$new_featured_content = get_theme_mod('featured_content');
+
+$empty     = ( empty( $home_headline ) && empty( $home_subheadline ) && empty( $home_content_area ) ) ? false : true;
+$emtpy_cta = ( empty( $cta_text ) ) ? false : true;
 
 ?>
 
@@ -20,8 +31,8 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 
 		<h1 class="featured-title">
 			<?php
-			if ( isset( $responsive_options['home_headline'] ) && $db && $empty )
-				echo $responsive_options['home_headline'];
+			if ( isset( $home_headline ) && $db && $empty )
+				echo get_theme_mod('home_headline');
 			else {
 				_e( 'Hello, World!', 'responsive' );
 			}
@@ -30,29 +41,29 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 
 		<h2 class="featured-subtitle">
 			<?php
-			if ( isset( $responsive_options['home_subheadline'] ) && $db && $empty )
-				echo $responsive_options['home_subheadline'];
+			if ( isset( $home_subheadline ) && $db && $empty )
+				echo get_theme_mod('home_subheadline');
 			else
 				_e( 'Your H2 subheadline here', 'responsive' );
 			?>
 		</h2>
 
 		<?php
-		if ( isset( $responsive_options['home_content_area'] ) && $db && $empty ) {
-			echo do_shortcode( wpautop( $responsive_options['home_content_area'] ) );
+		if ( isset( $home_content_area ) && $db && $empty ) {
+			echo do_shortcode( wpautop( get_theme_mod('home_content_area') ) );
 		} else {
 			echo '<p>' . __( 'Your title, subtitle and this very content is editable from Theme Option. Call to Action button and its destination link as well. Image on your right can be an image or even YouTube video if you like.', 'responsive' ) . '</p>';
 		} ?>
 
 
-		<?php if ( $responsive_options['cta_button'] == 0 ): ?>
+		<?php if ( get_theme_mod('cta_button') == 0 ): ?>
 
 			<div class="call-to-action">
 
-				<a href="<?php echo $responsive_options['cta_url']; ?>" class="blue button">
+				<a href="<?php echo get_theme_mod('cta_url'); ?>" class="blue button">
 					<?php
-					if ( isset( $responsive_options['cta_text'] ) && $db && $emtpy_cta )
-						echo $responsive_options['cta_text'];
+					if ( isset( $cta_text ) && $db && $emtpy_cta )
+						echo get_theme_mod('cta_text');
 					else
 						_e( 'Call to Action', 'responsive' );
 					?>
@@ -66,7 +77,7 @@ $emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
 
 	<div id="featured-image" class="grid col-460 fit">
 
-		<?php $featured_content = ( !empty( $responsive_options['featured_content'] ) ) ? $responsive_options['featured_content'] : '<img class="aligncenter" src="' . get_template_directory_uri() . '/core/images/featured-image.png" width="440" height="300" alt="" />'; ?>
+		<?php $featured_content = ( !empty( $new_featured_content ) ) ? $new_featured_content : '<img class="aligncenter" src="' . get_template_directory_uri() . '/core/images/featured-image.png" width="440" height="300" alt="" />'; ?>
 
 		<?php echo do_shortcode( wpautop( $featured_content ) ); ?>
 
