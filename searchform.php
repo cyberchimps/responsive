c99669b17007252c8f4c9a55f8946df506aa0ec8
Vallari Bakhle <vallari.bakhle@hbwsl.com>
2016-10-06 13:29:33 +0530

    esc functions added

--- searchform.php
+++ searchform.php
@@ -20,7 +20,7 @@ if ( !defined( 'ABSPATH' ) ) {
  * @since          available since Release 1.0
  */
 ?>
-<form method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="s"><?php esc_attr_e( 'Search for:', 'responsive' ) ?></label>
 	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'search here &hellip;', 'responsive' ); ?>" />
 	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Go', 'responsive' ); ?>" />
