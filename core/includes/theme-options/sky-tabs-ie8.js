a39137216c8503e8e1bc325be7a5d4ea96e6f2c0
Vallari Bakhle <vallari.bakhle@hbwsl.com>
2017-03-30 10:43:24 +0530

    fixed theme options issue in safari and version updated-2.3

--- core/includes/theme-options/sky-tabs-ie8.js
+++ core/includes/theme-options/sky-tabs-ie8.js
@@ -15,38 +15,56 @@ jQuery(function()
 		
 	});
 });
-jQuery(document).ready(function ($) {
-	jQuery('#form').submit( function (e) {	
-		tinyMCE.triggerSave();
-		var a = $(document.activeElement.id).selector;		
-	    if (a == "responsive_theme_options[submit]") {
-	        var b =  $(this).serialize();	       
-	        jQuery.post( 'options.php', b ).error( 
-	            function() {
-	                
-	            }).success( function() {
-	            	
-	            	var html = '<div class="formsuccess"><p><strong>Options Saved</strong></p></div>';
-	            	$(html).hide().appendTo(".sky-tabs").fadeIn(400).delay(1200).fadeOut(600);	            	
-           	            	
-	            });
-	            return false;   
-	    } 
-	    else 
-	    {
-	    var b =  $(this).serialize(); 
-	    b=b+'&responsive_theme_options%5Breset%5D=Restore Defaults'; 	    
-    	jQuery.post( 'options.php', b ).error( 
-        function() {            
-        }).success( function() {
-        	var html = '<div class="formsuccess"><p><strong>Options Reset</strong></p></div>';
-        	$(html).hide().appendTo(".sky-tabs").fadeIn(400).delay(1200).fadeOut(600);
-        	setTimeout(function(){
-        	    location.reload();
-        	},900);
-        	
-        });
-        return false; 
-	    }
-        });
-});
+jQuery(function($) {
+      var submitA = null;
+      var $form = $('#form');
+      var $submitB = $form.find('input[type=submit]');
+
+      $form.submit(function(event) {
+
+		  tinyMCE.triggerSave();
+          if (null === submitA) {
+              // If no button is explicitly clicked, the browser will automatically choose the first in source-order (same is done here)
+              submitA = $submitB[0];
+
+          }
+
+			if(submitA.name == "responsive_theme_options[submit]")
+			{
+				var b =  $(this).serialize();	       
+			    jQuery.post( 'options.php', b ).error( 
+			        function() {
+			            
+			        }).success( function() {
+			        	
+			        	var html = '<div class="formsuccess"><p><strong>Options Saved</strong></p></div>';
+			        	$(html).hide().appendTo(".sky-tabs").fadeIn(400).delay(1200).fadeOut(600);	            	
+		       	            	
+			        });
+			        return false;
+			}
+			if(submitA.name == "responsive_theme_options[reset]")
+			{
+				var b =  $(this).serialize(); 
+				b=b+'&responsive_theme_options%5Breset%5D=Restore Defaults'; 	    
+				jQuery.post( 'options.php', b ).error( 
+				function() {            
+				}).success( function() {
+					var html = '<div class="formsuccess"><p><strong>Options Reset</strong></p></div>';
+					$(html).hide().appendTo(".sky-tabs").fadeIn(400).delay(1200).fadeOut(600);
+					setTimeout(function(){
+					    location.reload();
+					},900);
+					
+				});
+				return false; 
+			}
+          
+
+          return false;
+      });
+
+      $submitB.click(function(event) {
+          submitA = this;
+      });
+  });
