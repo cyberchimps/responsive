2e68ccc1a8c28e0fb49bad2a06b19ea1142e5df6
milind6652 <milind@apples-MacBook-Air-2.local>
2018-09-25 15:14:18 +0530

    Fixed outdated template issue (v3.4.5)

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -13,116 +13,111 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @version     3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+defined('ABSPATH') || exit;
 
-get_header( 'shop' ); 
+get_header('shop');
 
-	$responsive_options = responsive_get_options();
-	if ( isset($responsive_options['override_woo']) && 1 == $responsive_options['override_woo'] )
-	{
-	wp_enqueue_style( 'custom_shop_style', get_template_directory_uri() . '/woocommerce/shop.css' );
-	wp_enqueue_style( 'fontawesome-style', get_template_directory_uri() . '/core/css/font-awesome.min.css', false, '4.7.0');
-	
-	?>
+    $responsive_options = responsive_get_options();
+    if (isset($responsive_options['override_woo']) && 1 == $responsive_options['override_woo']) {
+        wp_enqueue_style('custom_shop_style', get_template_directory_uri() . '/woocommerce/shop.css');
+        wp_enqueue_style('fontawesome-style', get_template_directory_uri() . '/core/css/font-awesome.min.css', false, '4.7.0'); ?>
 
 	<?php
-	}
-		/**
-		 * woocommerce_before_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 * @hooked WC_Structured_Data::generate_website_data() - 30
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
+    }
+        /**
+         * Hook: woocommerce_before_main_content.
+         *
+         * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+         * @hooked woocommerce_breadcrumb - 20
+         * @hooked WC_Structured_Data::generate_website_data() - 30
+         */
+        do_action('woocommerce_before_main_content');
+    ?>
 
     <header class="woocommerce-products-header">
 
-		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+		<?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
 
 			<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 
 		<?php endif; ?>
 
 		<?php
-			/**
-			 * woocommerce_archive_description hook.
-			 *
-			 * @hooked woocommerce_taxonomy_archive_description - 10
-			 * @hooked woocommerce_product_archive_description - 10
-			 */
-			do_action( 'woocommerce_archive_description' );
-		?>
+            /**
+             * Hook: woocommerce_archive_description.
+             *
+             * @hooked woocommerce_taxonomy_archive_description - 10
+             * @hooked woocommerce_product_archive_description - 10
+             */
+            do_action('woocommerce_archive_description');
+        ?>
 
     </header>
 
-		<?php if ( have_posts() ) { ?>
+		<?php if (have_posts()) {
+            ?>
 
 			<?php
-				/**
-				 * woocommerce_before_shop_loop hook.
-				 *
-				 * @hooked wc_print_notices - 10
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
-				do_action( 'woocommerce_before_shop_loop' );
-				
-				woocommerce_product_loop_start();
-				
-				if ( wc_get_loop_prop( 'total' ) ) {
-						
-					while ( have_posts() ) { 
-						the_post(); 
+                /**
+                 * Hook: woocommerce_before_shop_loop.
+                 *
+                 * @hooked wc_print_notices - 10
+                 * @hooked woocommerce_result_count - 20
+                 * @hooked woocommerce_catalog_ordering - 30
+                 */
+                do_action('woocommerce_before_shop_loop');
 
-						/**
-						 * woocommerce_shop_loop hook.
-						 *
-						 * @hooked WC_Structured_Data::generate_product_data() - 10
-						 */
-						do_action( 'woocommerce_shop_loop' );
-						wc_get_template_part( 'content', 'product' );
-					}
-				}
-				woocommerce_product_loop_end();
-			
-				/**
-				 * woocommerce_after_shop_loop hook.
-				 *
-				 * @hooked woocommerce_pagination - 10
-				 */
-				do_action( 'woocommerce_after_shop_loop' );
-		}else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-		do_action( 'woocommerce_no_products_found' );
-	}
+            woocommerce_product_loop_start();
 
-		/** Hook: woocommerce_after_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	
-		/**
-		 *  Hook: woocommerce_sidebar hook.
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action( 'woocommerce_sidebar' );
-	
-	if ( isset($responsive_options['override_woo']) && 1 == $responsive_options['override_woo'] )
-	{
-	?>
+            if (wc_get_loop_prop('total')) {
+                while (have_posts()) {
+                    the_post();
 
-<?php } ?>
-<?php get_footer( 'shop' ); ?>
+                    /**
+                     * Hook: woocommerce_shop_loop.
+                     *
+                     * @hooked WC_Structured_Data::generate_product_data() - 10
+                     */
+                    do_action('woocommerce_shop_loop');
+                    wc_get_template_part('content', 'product');
+                }
+            }
+            woocommerce_product_loop_end();
+
+            /**
+             * Hook: woocommerce_after_shop_loop.
+             *
+             * @hooked woocommerce_pagination - 10
+             */
+            do_action('woocommerce_after_shop_loop');
+        } else {
+            /**
+             * Hook: woocommerce_after_main_content.
+             *
+             * @hooked wc_no_products_found - 10
+             */
+            do_action('woocommerce_no_products_found');
+        }
+
+        /** Hook: woocommerce_sidebar.
+         *
+         * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+         */
+        do_action('woocommerce_after_main_content');
+
+        /**
+         *  Hook: woocommerce_sidebar hook.
+         *
+         * @hooked woocommerce_get_sidebar - 10
+         */
+        do_action('woocommerce_sidebar');
+
+    if (isset($responsive_options['override_woo']) && 1 == $responsive_options['override_woo']) {
+        ?>
+
+<?php
+    } ?>
+<?php get_footer('shop'); ?>
