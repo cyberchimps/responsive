344907a8208ad099ca9692810aa9a917da1e2a63
kajal-masal <kajal.masal@hbwsl.com>
2019-07-07 09:54:49 +0530

    movied footer copyrights into theme options

--- core/includes/customizer/customizer.php
+++ core/includes/customizer/customizer.php
@@ -74,32 +74,7 @@ if (!($wp_version >= 4.7))
 }
 
 
-/*------------------------------------------------------------------
-	// Copyright Text
--------------------------------------------------------------------*/
 
-$wp_customize->add_section( 'footer_section', array(
-		'title'                 => __( 'Footer Settings', 'responsive' ),
-		'priority'              => 30
-	) );
-	$wp_customize->add_setting( 'responsive_theme_options[copyright_textbox]',array( 'default' => __('Default copyright text','responsive'),'sanitize_callback' => 'wp_filter_nohtml_kses', 'type' => 'option' ) );
-
-	$wp_customize->add_control( 'res_copyright_textbox', array(
-		'label'                 => __( 'Copyright text', 'responsive' ),
-		'section'               => 'footer_section',
-		'settings'              => 'responsive_theme_options[copyright_textbox]',
-		'type'                  => 'text',
-		'priority' => 1
-	) );
-
-	$wp_customize->add_setting( 'responsive_theme_options[poweredby_link]', array( 'sanitize_callback' => 'responsive_sanitize_checkbox', 'type' => 'option' ) );
-	$wp_customize->add_control( 'res_poweredby_link', array(
-		'label'                 => __( 'Display Powered By WordPress Link', 'responsive' ),
-		'section'               => 'footer_section',
-		'settings'              => 'responsive_theme_options[poweredby_link]',
-		'type'                  => 'checkbox',
-		'priority' => 2
-	) );
 
 
 }
@@ -239,6 +214,7 @@ function register_options() {
 		'home-page',
 		'links',
 		'footer',
+		'footer-copyrights',
 	);
 
 	foreach ( $files as $key ) {
