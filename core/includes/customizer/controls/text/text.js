77c9ef920667c2ccc956522b613b4dc8acf99f9e
prajakti <prajakti@hbwsladmins-MacBook-Pro.local>
2019-07-11 19:48:25 +0530

    phpcs js errors fixed

--- core/includes/customizer/controls/text/text.js
+++ core/includes/customizer/controls/text/text.js
@@ -1,25 +1,47 @@
-wp.customize.controlConstructor['responsive-text'] = wp.customize.Control.extend({
+/**
+ * Text JS
+ *
+ * @package Responsive WordPress theme
+ *
+ * This file includes several helper functions and the core control JS.
+ */
 
-	// When we're finished loading continue processing.
-	ready: function() {
+wp.customize.controlConstructor['responsive-text'] = wp.customize.Control.extend(
+	{
 
-		'use strict';
+		// When we're finished loading continue processing.
+		ready: function() {
 
-		var control = this;
+			'use strict';
 
-		// Save the values
-		control.container.on( 'change keyup paste', '.desktop input', function() {
-			control.settings['desktop'].set( jQuery( this ).val() );
-		} );
+			var control = this;
 
-		control.container.on( 'change keyup paste', '.tablet input', function() {
-			control.settings['tablet'].set( jQuery( this ).val() );
-		} );
+			// Save the values.
+			control.container.on(
+				'change keyup paste',
+				'.desktop input',
+				function() {
+					control.settings['desktop'].set( jQuery( this ).val() );
+				}
+			);
 
-		control.container.on( 'change keyup paste', '.mobile input', function() {
-			control.settings['mobile'].set( jQuery( this ).val() );
-		} );
+			control.container.on(
+				'change keyup paste',
+				'.tablet input',
+				function() {
+					control.settings['tablet'].set( jQuery( this ).val() );
+				}
+			);
+
+			control.container.on(
+				'change keyup paste',
+				'.mobile input',
+				function() {
+					control.settings['mobile'].set( jQuery( this ).val() );
+				}
+			);
+
+		}
 
 	}
-
-});
+);
