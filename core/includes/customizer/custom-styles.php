53e8bba3525a421be4811645c57f3d7e25c5ddbc
Manish Verma <manish.verma@hbwsl.com>
2019-12-17 10:58:04 +0000

    Merged in manishvermahbwsl/responsive/restructure4.0 (pull request #275)
    Fixed gutenberg cover issue  , fixed alignwide alignfull issue
    
    * added new features in for 4.0
    
    * Added new feature for editor color sync with frontend and sidebar width range controller
    
    * Added new feature for editor color sync with frontend and sidebar width range controller
    
    * added new feature for header width CYB-1072
    
    * added options for meta typography,blog padding, masonry view, show thumbnail without padding on archive page
    
    * added 4 footer sidebars
    
    * Rename Top Widget to header widget
    
    * removed top widget and added header widgets instead
    
    * Added same design to archive search and category page as blog page
    
    * added padding controller for blog entry in customizer
    
    * Fixed padding, and background issues
    
    * wide align and align full blocks issue and customizer changes apply on gutenberg button
    
    * Added header and footer widgets styling
    
    * Added button padding options
    
    * Added button padding options
    
    * removed html validator errors
    
    * removed html validator errors
    
    * removed html validator errors
    
    * removed html validator errors
    
    * Fixed notice for undefined font-size and Added meta styles
    
    * ignored codeconception folder
    
    * fixed customizer fa fa icon display issue
    
    * fixed customizer fa fa icon display issue
    
    * fixed issue of container layout option empty
    
    * Fixed meta ascii code print issue CYB-1272
    
    * Fixed CYB-1263
    
    * Fixed CYB-1263
    
    * Fixed CYB-1263
    
    * fixed CYB-1273 CYB-1214 CYB-1259
    
    * Fixed CYB-1278
    
    * Get Latest merges and resolved conflict
    
    * Fixed CYB-1275
    
    * resolved preview js conflict
    
    * resolved preview js conflict
    
    * Fixed alignwide and content-boxed and blog entries page title and bckground issue with fullwidth content, content-boxed, boxed layouts
    
    * resolved conflict
    
    * Fixed CYB-1284 to move blog title enable disable to blog entries
    
    * resolved conflict
    
    * resolved social icon changes
    
    * added default copyright content as site title
    
    * fixed container padding issue for alingwide element in mobile and tablet view
    
    * Added icon for comments in meta
    
    * Fixed google font first time load issue in customizer
    
    * resolved conflict
    
    * resolved conflict
    
    * resolved conflict
    
    * Fixed css to display every things end to end in full width streched layout
    
    * fixed search no result page
    
    * added max-width container width to elements inside alignfull and alignwide
    
    * added max-width container width to elements inside alignfull and alignwide
    
    * Moved Site title and tagline above menu in General typography
    
    * Changed customizer label footer layout to footer widget layout
    
    * Removed DEFAULT Header Layout option and changed position on header menu to header layout from menu
    
    * Fixed woocommerce sidebar width adjustment issue
    
    * Removed DEFAULT Header Layout option
    
    * fixed sidebar in customizer issue, blog title and content padding issue for blog entry
    
    * Fixed gutenberg cover issue
    
    * Fixed gutenberg cover issue
    
    * fixed alignwide alignfull issue
    
    Approved-by: Kajal Masal <kajal.masal@hbwsl.com>

--- core/includes/customizer/custom-styles.php
+++ core/includes/customizer/custom-styles.php
@@ -529,8 +529,7 @@ function responsive_premium_custom_color_styles() {
 			margin-left: auto;
 			margin-right: auto;
 		}
-
-		.wp-block-cover__inner-container,.alignfull *:not(.alignfull), .alignfull *:not(.alignwide), .alignwide *:not(.alignwide), .alignwide *:not(.alignfull),
+		.wp-block-cover__inner-container,
 		.boxed-layout	.content-area, body.default-layout .content-outer, body.full-width-layout .content-outer, body.full-width-no-box .content-outer, #footer .content-outer{
 			max-width: {$container_width}px;
 		}
