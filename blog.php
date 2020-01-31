f688c01d29841c33e2197dae45aad43f21412f4f
Ratnesh Kadam <ratnesh.kadam@hbwsl.com>
2020-01-31 11:05:21 +0530

    Feature - code compatible with WordPress coding standards

--- blog.php
+++ blog.php
@@ -6,7 +6,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 /**
@@ -27,92 +27,91 @@ Template Name: Blog (full posts) (Deprecated)
 
 get_header();
 
-$more = 0;
 ?>
 <?php responsive_wrapper_top(); // before wrapper content hook. ?>
 <div id="wrapper" class="site-content clearfix">
 
-	<div class="content-outer container">
-		<div class="row">
-			<?php responsive_in_wrapper(); // wrapper hook. ?>
-			<main id="primary" class="content-area <?php echo esc_attr( implode( ' ', responsive_get_content_classes() ) ); ?>">
-				<?php get_template_part( 'loop-header', get_post_type() ); ?>
-				<?php
-				if ( get_query_var( 'paged' ) ) {
-					$responsive_paged = get_query_var( 'paged' );
-				} elseif ( get_query_var( 'page' ) ) {
-					$responsive_paged = get_query_var( 'page' );
-				} else {
-					$responsive_paged = 1;
-				}
-				$responsive_blog_query = new WP_Query(
-					array(
-						'post_type' => 'post',
-						'paged'     => $responsive_paged,
-					)
-				);
+    <div class="content-outer container">
+        <div class="row">
+            <?php responsive_in_wrapper(); // wrapper hook. ?>
+            <main id="primary" class="content-area <?php echo esc_attr( implode( ' ', responsive_get_content_classes() ) ); ?>">
+                <?php get_template_part( 'loop-header', get_post_type() ); ?>
+                <?php
+                if ( get_query_var( 'paged' ) ) {
+                    $responsive_paged = get_query_var( 'paged' );
+                } elseif ( get_query_var( 'page' ) ) {
+                    $responsive_paged = get_query_var( 'page' );
+                } else {
+                    $responsive_paged = 1;
+                }
+                $responsive_blog_query = new WP_Query(
+                    array(
+                        'post_type' => 'post',
+                        'paged'     => $responsive_paged,
+                    )
+                );
 
-				if ( $responsive_blog_query->have_posts() ) :
+                if ( $responsive_blog_query->have_posts() ) :
 
-					while ( $responsive_blog_query->have_posts() ) :
-						$responsive_blog_query->the_post();
-						?>
+                    while ( $responsive_blog_query->have_posts() ) :
+                        $responsive_blog_query->the_post();
+                        ?>
 
-						<?php responsive_entry_before(); ?>
-						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<?php responsive_entry_top(); ?>
+                        <?php responsive_entry_before(); ?>
+                        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <?php responsive_entry_top(); ?>
 
-							<?php get_template_part( 'post-meta', get_post_type() ); ?>
+                            <?php get_template_part( 'post-meta', get_post_type() ); ?>
 
-							<div class="post-entry">
-								<?php if ( has_post_thumbnail() ) : ?>
-									<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" <?php responsive_schema_markup( 'url' ); ?>>
-										<?php the_post_thumbnail(); ?>
-									</a>
-								<?php endif; ?>
-								<?php the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
-								<?php
-								wp_link_pages(
-									array(
-										'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ),
-										'after'  => '</div>',
-									)
-								);
-								?>
-							</div><!-- end of .post-entry -->
+                            <div class="post-entry">
+                                <?php if ( has_post_thumbnail() ) : ?>
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" <?php responsive_schema_markup( 'url' ); ?>>
+                                        <?php the_post_thumbnail(); ?>
+                                    </a>
+                                <?php endif; ?>
+                                <?php the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
+                                <?php
+                                wp_link_pages(
+                                    array(
+                                        'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ),
+                                        'after'  => '</div>',
+                                    )
+                                );
+                                ?>
+                            </div><!-- end of .post-entry -->
 
-							<?php get_template_part( 'post-data', get_post_type() ); ?>
+                            <?php get_template_part( 'post-data', get_post_type() ); ?>
 
-							<?php responsive_entry_bottom(); ?>
-						</div><!-- end of #post-<?php the_ID(); ?> -->
-						<?php responsive_entry_after(); ?>
+                            <?php responsive_entry_bottom(); ?>
+                        </div><!-- end of #post-<?php the_ID(); ?> -->
+                        <?php responsive_entry_after(); ?>
 
-						<?php
-					endwhile;
+                    <?php
+                    endwhile;
 
-					if ( $responsive_blog_query->max_num_pages > 1 ) :
-						?>
-						<div class="navigation">
-							<div class="previous"><?php next_posts_link( __( '&#8249; Older posts', 'responsive' ), $responsive_blog_query->max_num_pages ); ?></div>
-							<div class="next"><?php previous_posts_link( __( 'Newer posts &#8250;', 'responsive' ), $responsive_blog_query->max_num_pages ); ?></div>
-						</div><!-- end of .navigation -->
-						<?php
-					endif;
+                    if ( $responsive_blog_query->max_num_pages > 1 ) :
+                        ?>
+                        <div class="navigation">
+                            <div class="previous"><?php next_posts_link( __( '&#8249; Older posts', 'responsive' ), $responsive_blog_query->max_num_pages ); ?></div>
+                            <div class="next"><?php previous_posts_link( __( 'Newer posts &#8250;', 'responsive' ), $responsive_blog_query->max_num_pages ); ?></div>
+                        </div><!-- end of .navigation -->
+                    <?php
+                    endif;
 
-					else :
+                else :
 
-						get_template_part( 'loop-no-posts' );
+                    get_template_part( 'loop-no-posts' );
 
-				endif;
-					wp_reset_postdata();
-					?>
+                endif;
+                wp_reset_postdata();
+                ?>
 
-			</main><!-- end of #primary -->
+            </main><!-- end of #primary -->
 
-			<?php get_sidebar(); ?>
-		</div>
-	</div>
-<?php responsive_wrapper_bottom(); // after wrapper content hook. ?>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+    <?php responsive_wrapper_bottom(); // after wrapper content hook. ?>
 </div> <!-- end of #wrapper -->
 <?php responsive_wrapper_end(); // after wrapper hook. ?>
 <?php get_footer(); ?>
