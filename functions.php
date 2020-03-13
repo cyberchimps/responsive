a7925dea0dba8c1c6c560a625534903400576b5b
Ratnesh Kadam <ratnesh.kadam@hbwsl.com>
2020-03-13 16:16:57 +0000

    Merged in dev-responsive-cleanup (pull request #674)
    Dev responsive cleanup
    
    * Merge branch 'dev-responsive-cleanup' of bitbucket.org:cyberchimps/responsive into dev-responsive-cleanup
    
    * Language tranaslations
    
    * Merge branch 'dev-responsive-cleanup' of bitbucket.org:cyberchimps/responsive into dev-responsive-cleanup
    
    * language translations
    
    * Merge branch 'dev-responsive-cleanup' of bitbucket.org:cyberchimps/responsive into dev-responsive-cleanup
    
    * Language translations
    
    * Merge branch 'dev-responsive-cleanup' of bitbucket.org:cyberchimps/responsive into dev-responsive-cleanup
    
    * Translations
    
    * Merge branch 'dev-responsive-cleanup' of bitbucket.org:cyberchimps/responsive into dev-responsive-cleanup
    
    * Updated version number 4.1.8

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define constants.
  */
-define( 'RESPONSIVE_THEME_VERSION', '4.1.7' );
+define( 'RESPONSIVE_THEME_VERSION', '4.1.8' );
 define( 'RESPONSIVE_THEME_DIR', trailingslashit( get_template_directory() ) );
 define( 'RESPONSIVE_THEME_URI', trailingslashit( esc_url( get_template_directory_uri() ) ) );
 
