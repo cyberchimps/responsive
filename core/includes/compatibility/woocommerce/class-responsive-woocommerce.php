0f4ec978593f91c61c016c53275e8287daa085ff
Ratnesh Kadam <ratnesh.kadam@hbwsl.com>
2020-03-16 06:01:50 +0000

    Merged in bugfix/CYB-1613 (pull request #683)
    Bugfix/CYB-1613
    
    * CYB-1613 On fresh install, cross-sells do not display on cart page even after cross-sells are enabled.
    
    * CYB-1613 On fresh install, cross-sells do not display on cart page even after cross-sells are enabled.
    
    * CYB-1613 On fresh install, cross-sells do not display on cart page even after cross-sells are enabled.

--- core/includes/compatibility/woocommerce/class-responsive-woocommerce.php
+++ core/includes/compatibility/woocommerce/class-responsive-woocommerce.php
@@ -102,8 +102,7 @@ if ( ! class_exists( 'Responsive_Woocommerce' ) ) :
 		 * Disables Upsells
 		 */
 		public function cart_page_upselles() {
-
-			$crossselles_enabled = get_theme_mod( 'responsive_enable_crosssells_options' );
+			$crossselles_enabled = get_theme_mod( 'responsive_enable_crosssells_options', 1 );
 			if ( ! $crossselles_enabled ) {
 				remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
 			}
