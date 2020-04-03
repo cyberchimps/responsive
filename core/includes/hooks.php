eca9f5cdfa950dc8dbfaa858ddd968f4fd0ebeaa
Manish Verma <manish.verma@hbwsl.com>
2020-04-03 13:01:46 +0000

    Merged in bugfix/CYB-1738 (pull request #766)
    Added option to change link of logo / site title
    
    * Added option to change link of logo / site title
    
    * removed error log

--- core/includes/hooks.php
+++ core/includes/hooks.php
@@ -393,3 +393,43 @@ function responsive_no_js_class() {
 }
 
 add_action( 'wp_head', 'responsive_no_js_class' );
+/**
+ * Change the custom logo URL
+ */
+function responsive_custom_logo_link() {
+
+	// The logo.
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+
+	$html = '';
+
+	// If has logo.
+	if ( $custom_logo_id ) {
+
+		// Attr.
+		$custom_logo_attr = array(
+			'class'    => 'custom-logo',
+			'itemprop' => 'logo',
+		);
+
+		// Image alt.
+		$image_alt = get_post_meta( $custom_logo_id, '_wp_attachment_image_alt', true );
+		if ( empty( $image_alt ) ) {
+			$custom_logo_attr['alt'] = get_bloginfo( 'name', 'display' );
+		}
+
+		$custom_logo_url = get_theme_mod( 'responsive_custom_logo_url', home_url( '/' ) );
+
+		// Get the image.
+		$html = sprintf(
+			'<a href="%1$s" class="custom-logo-link" rel="home" itemprop="url">%2$s</a>',
+			esc_url( $custom_logo_url ),
+			wp_get_attachment_image( $custom_logo_id, 'full', false, $custom_logo_attr )
+		);
+
+	}
+
+	// Return.
+	return $html;
+}
+add_filter( 'get_custom_logo', 'responsive_custom_logo_link' );
