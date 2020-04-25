d77112f397a92d239190d2c563eaa3d4fd7fa4bb
Ratnesh Kadam <ratnesh.kadam@hbwsl.com>
2020-04-25 06:43:15 +0000

    Merged in dev-responsive-cleanup (pull request #834)
    Dev responsive cleanup
    
    * Merge branch 'dev-responsive-cleanup' of bitbucket.org:cyberchimps/responsive into dev-responsive-cleanup
    
    * Merge remote-tracking branch 'upstream/theme-check' into dev-responsive-cleanup
    
    * PHPUnit test cases
    
    * Ignored tests folder during phpcs check
    
    * CircleCI configuration
    
    * Test
    
    * Deleted bitbucket-pipelines.yml file
    
    * Shell script to setup WordPress for test environment
    
    * Merge branch 'dev-responsive-cleanup' of bitbucket.org:cyberchimps/responsive into dev-responsive-cleanup
    
    * Commit to check the circleci integration

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -15,6 +15,6 @@ class SampleTest extends WP_UnitTestCase {
 	 */
 	public function test_sample() {
 		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		$this->assertFalse( false );
 	}
 }
