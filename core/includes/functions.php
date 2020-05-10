95f2ecc5885026f29dac7cb60b7f02dba75be7f3
Prajakti Sahay <prajakti.sahay@hbwsl.com>
2020-05-10 05:52:10 +0000

    Merged in dev-responsive-cleanup (pull request #872)
    fatal error issue for wp_body_open function
    
    * added new feature for site branding display
    
    * conflicts resolved
    
    * Merge branch 'dev-responsive-cleanup' of https://bitbucket.org/cyberchimps/responsive into dev-responsive-cleanup
    
    * Merge branch 'dev-responsive-cleanup' of https://bitbucket.org/cyberchimps/responsive into dev-responsive-cleanup
    
    * Merge branch 'dev-responsive-cleanup' of https://bitbucket.org/cyberchimps/responsive into dev-responsive-cleanup
    
    * Merge branch 'dev-responsive-cleanup' of https://bitbucket.org/cyberchimps/responsive into dev-responsive-cleanup
    
    * Merge branch 'dev-responsive-cleanup' of https://bitbucket.org/cyberchimps/responsive into dev-responsive-cleanup
    
    * Merge branch 'dev-responsive-cleanup' of https://bitbucket.org/cyberchimps/responsive into dev-responsive-cleanup
    
    * Merge branch 'dev-responsive-cleanup' of https://bitbucket.org/cyberchimps/responsive into dev-responsive-cleanup
    
    * fatal error issue for wp_body_open function
    
    Approved-by: Ratnesh Kadam

--- core/includes/functions.php
+++ core/includes/functions.php
@@ -778,19 +778,7 @@ function responsive_add_pro_button() {
 	<?php
 }
 
-if ( ! function_exists( 'wp_body_open' ) ) {
-	/**
-	 * Fire the wp_body_open action.
-	 *
-	 * Added for backwards compatibility to support WordPress versions prior to 5.2.0.
-	 */
-	function wp_body_open() {
-		/**
-		 * Triggered after the opening <body> tag.
-		 */
-		do_action( 'wp_body_open' );
-	}
-}
+
 if ( ! function_exists( 'responsive_is_transparent_header' ) ) {
 	/**
 	 * Returns true if transparent header is enabled
--- functions.php
+++ functions.php
@@ -899,3 +899,17 @@ function responsive_header_widget_position() {
 
 }
 add_action( 'wp_head', 'responsive_header_widget_position' );
+
+if ( ! function_exists( 'wp_body_open' ) ) {
+	/**
+	 * Fire the wp_body_open action.
+	 *
+	 * Added for backwards compatibility to support WordPress versions prior to 5.2.0.
+	 */
+	function wp_body_open() {
+		/**
+		 * Triggered after the opening <body> tag.
+		 */
+		do_action( 'wp_body_open' );
+	}
+}
