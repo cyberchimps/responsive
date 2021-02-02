1816f87355e873ed49a751b3499bb29fadbcd70a
Adwait Deshpande <adwait.deshpande@hbwsl.com>
2021-02-02 09:31:05 +0000

    Merged in feature/RESPTHEME-84 (pull request #1207)
    Feature/RESPTHEME-84
    
    * RESPTHEME-83: Added functionality for Blog/Archive Date Box
    
    * RESPTHEME-83: Changed get_the_date value of day in date box
    
    * RESPTHEME-84: Added function for previewing date box font color
    
    * Merge branch 'develop' of https://bitbucket.org/cyberchimps/responsive into feature/RESPTHEME-84
    
    * RESPTHEME-84: Changed the position of else statement to line 29
    
    * Merge branch 'develop' of https://bitbucket.org/cyberchimps/responsive into feature/RESPTHEME-84
    
    * RESPTHEME-84: Resolved conflict with develop branch
    
    Approved-by: Ratnesh Kadam
    Approved-by: Nayan Gaikwad

--- core/includes/customizer/assets/js/customize-preview-color-control.js
+++ core/includes/customizer/assets/js/customize-preview-color-control.js
@@ -246,6 +246,28 @@
         } );
     } );
 
+    //Date box Font Color
+    api( 'responsive_link_color', function( value ) {
+        value.bind( function( newval ) {
+            let backgroundColor = newval;
+            hexcolor = backgroundColor.replace("#", "");
+            let r = parseInt(hexcolor.substr(0,2),16);
+            let g = parseInt(hexcolor.substr(2,2),16);
+            let b = parseInt(hexcolor.substr(4,2),16);
+            let o = Math.round(((parseInt(r) * 299) + (parseInt(g) * 587) + (parseInt(b) * 114)) /1000);
+            if( o > 125 ) {
+                $('.date-box-day').css("color", "black");
+                $('.date-box-month').css("color", "black");
+                $('.date-box-year').css("color", "black");
+            }
+            else {
+                $('.date-box-day').css("color", "white");   
+                $('.date-box-month').css("color", "white");
+                $('.date-box-year').css("color", "white");
+            }
+        } );
+    } );
+
     //Link Hover Color
     api( 'responsive_link_hover_color', function( value ) {
         value.bind( function( newval ) {
