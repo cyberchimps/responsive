56e7e085a6dc614dc0becdf0d4affcb56d552b50
Saurabh Bagde <bagdesaurabh69@gmail.com>
2021-10-26 17:20:35 +0530

    Builder moved to customize preview

--- core/includes/customizer/extend-controls/src/builder-layout/add-component.js
+++ core/includes/customizer/extend-controls/src/builder-layout/add-component.js
@@ -3,100 +3,99 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import ResponsiveControl from '../common/responsive.js';
 import Icons from '../common/icons.js';
+import { useState } from 'react';
 
 const { __ } = wp.i18n;
 
 const { ButtonGroup, Dashicon, Popover, Tooltip, Button } = wp.components;
 
 const { Component, Fragment } = wp.element;
-class AddComponent extends Component {
-	constructor() {
-		super( ...arguments );
-		this.addItem = this.addItem.bind( this );
-		this.state = {
-			isVisible: false,
-		}
-	}
-	addItem( item, row, column ) {
-		this.setState( { isVisible: false } );
-		let updateItems = this.props.list;
-		let theitem = [ {
+const AddComponent = props => {
+
+	const [state, setState] = useState({
+		isVisible: false,
+	});
+
+	const addItem = (item, row, column) => {
+		setState({ isVisible: false });
+		let updateItems = props.list;
+		let theitem = [{
 			id: item,
-		} ];
-		updateItems.push( {
+		}];
+		updateItems.push({
 			id: item,
 		});
-		this.props.setList( updateItems );
+		props.setList(updateItems);
 	}
-	render() {
-		const renderItems = ( item, row, column ) => {
-			let available = true;
-			this.props.controlParams.rows.map( ( zone ) => {
-				Object.keys( this.props.settings[zone] ).map( ( area ) => {
-					if ( this.props.settings[zone][area].includes( item ) ) {
-						available = false;
-					}
-				} );
-			} );
-			return (
-				<Fragment>
-					{ available && (
-						<Button
-							isTertiary
-							className={ 'builder-add-btn' }
-							onClick={ () => {
-								this.addItem( item, row, column );
-							} }
-						>
-							{ ( undefined !== this.props.choices[ item ] && undefined !== this.props.choices[ item ].name ? this.props.choices[ item ].name : '' ) }
-						</Button>
-					) }
-				</Fragment>
-			);
-		};
-		const toggleClose = () => {
-			if ( this.state.isVisible === true ) {
-				this.setState( { isVisible: false } );
-			}
-		};
-		let classForAdd = 'responsive-builder-add-item';
 
-		if ( 'header_desktop_items' === this.props.controlParams.group && 'right' === this.props.location ) {
-			classForAdd = classForAdd + ' center-on-left';
-		}
-		if ( 'header_desktop_items' === this.props.controlParams.group && 'left' === this.props.location ) {
-			classForAdd = classForAdd + ' center-on-right';
-		}
-		if ( 'header_desktop_items' === this.props.controlParams.group && 'left_center' === this.props.location ) {
-			classForAdd = classForAdd + ' right-center-on-right';
-		}
-		if ( 'header_desktop_items' === this.props.controlParams.group && 'right_center' === this.props.location ) {
-			classForAdd = classForAdd + ' left-center-on-left';
-		}
-		return (
-			<div className={ classForAdd } key={ this.props.id }>
-				{ this.state.isVisible && (
-					<Popover position="top" className="responsive-popover-add-builder" onClose={ toggleClose }>
-						<div className="responsive-popover-builder-list">
-							<ButtonGroup className="responsive-radio-container-control">
-								{
-								Object.keys( this.props.choices ).map( ( item ) => {
-									return renderItems( item, this.props.row, this.props.column );
-								} ) }
-							</ButtonGroup>
-						</div>
-					</Popover>
-				) }
+	const renderItems = (item, row, column) => {
+		let available = true;
+		props.controlParams.rows.map((zone) => {
+			Object.keys(props.settings[zone]).map((area) => {
+				if (props.settings[zone][area].includes(item)) {
+					available = false;
+				}
+			});
+		});
+		return <>
+			{available && (
 				<Button
-					className="responsive-builder-item-add-icon"
-					onClick={ () => {
-						this.setState( { isVisible: true } );
-					} }
+					isTertiary
+					className={'builder-add-btn'}
+					onClick={() => {
+						addItem(item, row, column);
+					}}
 				>
-					<Dashicon icon="plus"/>
+					{(undefined !== props.choices[item] && undefined !== props.choices[item].name ? props.choices[item].name : '')}
 				</Button>
-			</div>
-		);
+			)}
+		</>
+
+	};
+	const toggleClose = () => {
+		if (state.isVisible === true) {
+			setState({ isVisible: false });
+		}
+	};
+	let classForAdd = 'responsive-builder-add-item';
+
+	if ('header_desktop_items' === props.controlParams.group && 'right' === props.location) {
+		classForAdd = classForAdd + ' center-on-left';
 	}
+	if ('header_desktop_items' === props.controlParams.group && 'left' === props.location) {
+		classForAdd = classForAdd + ' center-on-right';
+	}
+	if ('header_desktop_items' === props.controlParams.group && 'left_center' === props.location) {
+		classForAdd = classForAdd + ' right-center-on-right';
+	}
+	if ('header_desktop_items' === props.controlParams.group && 'right_center' === props.location) {
+		classForAdd = classForAdd + ' left-center-on-left';
+	}
+	return <>
+		<div className={classForAdd} key={props.id}>
+			{state.isVisible && (
+				<Popover position="top" className="responsive-popover-add-builder" onClose={toggleClose}>
+					<div className="responsive-popover-builder-list">
+						<ButtonGroup className="responsive-radio-container-control">
+							{
+								Object.keys(props.choices).map((item) => {
+									return renderItems(item, props.row, props.column);
+								})}
+						</ButtonGroup>
+					</div>
+				</Popover>
+			)}
+			<Button
+				className="responsive-builder-item-add-icon"
+				onClick={() => {
+					setState({ isVisible: true });
+				}}
+			>
+				<Dashicon icon="plus" />
+			</Button>
+		</div>
+	</>
+
+
 }
 export default AddComponent;
--- core/includes/customizer/extend-controls/src/builder-layout/row-component.js
+++ core/includes/customizer/extend-controls/src/builder-layout/row-component.js
@@ -9,62 +9,66 @@ const { __ } = wp.i18n;
 
 const { ButtonGroup, Dashicon, Tooltip, Button } = wp.components;
 
-const { Component, Fragment } = wp.element;
-class RowComponent extends Component {
-	render() {
-		let centerClass = 'no-center-items';
-		if ( 'header_desktop_items' === this.props.controlParams.group && typeof this.props.items[this.props.row + '_center'] != "undefined" && this.props.items[this.props.row + '_center'] != null && this.props.items[this.props.row + '_center'].length != null && this.props.items[this.props.row + '_center'].length > 0  ){
-			centerClass = 'has-center-items';
-		}
-		if ( 'popup' === this.props.row ) {
-			centerClass = 'popup-vertical-group';
-		}
-		if ( 'footer_items' === this.props.controlParams.group ) {
-			var columns = this.props.customizer.control( 'footer_' + this.props.row + '_columns' ).setting.get();
-			var layout = this.props.customizer.control( 'footer_' + this.props.row + '_layout' ).setting.get();
-			var direction = this.props.customizer.control( 'footer_' + this.props.row + '_direction' ).setting.get();
-			centerClass = 'footer-column-row footer-row-columns-' + columns + ' footer-row-layout-' + layout.desktop + ' footer-row-direction-' + direction.desktop;
-		}
-		const mode = ( this.props.controlParams.group.indexOf( 'header' ) !== -1 ? 'header' : 'footer' );
-		let besideItems = [];
-		return (
-			<div className={ `responsive-builder-areas ${ centerClass }` }>
-				<Button
-					className="responsive-row-left-actions"
-					aria-label={ __( 'Edit Settings for', 'responsive' ) + ' ' + ( this.props.row === 'popup' ? __( 'Off Canvas', 'responsive' ) : this.props.row + ' ' + __( 'Row', 'responsive' ) ) }
-					onClick={ () => this.props.focusPanel( mode + '_' + this.props.row ) }
-					icon="admin-generic"
-				>
-				</Button>
-				<Button
-					className="responsive-row-actions"
-					aria-label={ __( 'Edit Settings for', 'responsive' ) + ' ' + ( this.props.row === 'popup' ? __( 'Off Canvas', 'responsive' ) : this.props.row + ' ' + __( 'Row', 'responsive' ) ) }
-					onClick={ () => this.props.focusPanel( mode + '_' + this.props.row ) }
-				>
-					{ ( this.props.row === 'popup' ? __( 'Off Canvas', 'responsive' ) : this.props.row + ' ' + __( 'Row', 'responsive' ) ) }
-					<Dashicon icon="admin-generic" />
-				</Button>
-				<div className="responsive-builder-group responsive-builder-group-horizontal" data-setting={ this.props.row }>
-					{ Object.keys( this.props.controlParams.zones[ this.props.row ] ).map( ( zone, index ) => {
-						if ( this.props.row + '_left_center' === zone || this.props.row + '_right_center' === zone ) {
+const RowComponent = props => {
+	const {
+		customizer,
+		controlParams,
+		focusPanel,
+		removeItem
+	} = props;
+	const controlParamsGroup = controlParams.group;
+	let centerClass = 'no-center-items';
+	if ('header_desktop_items' === controlParamsGroup && typeof props.items[props.row + '_center'] != "undefined" && props.items[props.row + '_center'] != null && props.items[props.row + '_center'].length != null && props.items[props.row + '_center'].length > 0) {
+		centerClass = 'has-center-items';
+	}
+	if ('popup' === props.row) {
+		centerClass = 'popup-vertical-group';
+	}
+	if ('footer_items' === controlParamsGroup) {
+		var columns = customizer.control('footer_' + props.row + '_columns').setting.get();
+		var layout = customizer.control('footer_' + props.row + '_layout').setting.get();
+		var direction = customizer.control('footer_' + props.row + '_direction').setting.get();
+		centerClass = 'footer-column-row footer-row-columns-' + columns + ' footer-row-layout-' + layout.desktop + ' footer-row-direction-' + direction.desktop;
+	}
+	const mode = (controlParamsGroup.indexOf('header') !== -1 ? 'header' : 'footer');
+	let besideItems = [];
+	return <>
+		<div className={`responsive-builder-areas ${centerClass}`}>
+			<Button
+				className="responsive-row-left-actions"
+				aria-label={__('Edit Settings for', 'responsive') + ' ' + (props.row === 'popup' ? __('Off Canvas', 'responsive') : props.row + ' ' + __('Row', 'responsive'))}
+				onClick={() => focusPanel(mode + '_' + props.row)}
+				icon="admin-generic"
+			>
+			</Button>
+			<Button
+				className="responsive-row-actions"
+				aria-label={__('Edit Settings for', 'responsive') + ' ' + (props.row === 'popup' ? __('Off Canvas', 'responsive') : props.row + ' ' + __('Row', 'responsive'))}
+				onClick={() => focusPanel(mode + '_' + props.row)}
+			>
+				{(props.row === 'popup' ? __('Off Canvas', 'responsive') : props.row + ' ' + __('Row', 'responsive'))}
+				<Dashicon icon="admin-generic" />
+			</Button>
+			<div className="responsive-builder-group responsive-builder-group-horizontal" data-setting={props.row}>
+				{Object.keys(controlParams.zones[props.row]).map((zone, index) => {
+					if (props.row + '_left_center' === zone || props.row + '_right_center' === zone) {
+						return;
+					}
+					if ('footer_items' === controlParamsGroup) {
+						if (columns < (index + 1)) {
 							return;
 						}
-						if ( 'footer_items' === this.props.controlParams.group ) {
-							if ( columns < ( index + 1 ) ) {
-								return;
-							}
-						}
-						if ( 'header_desktop_items' === this.props.controlParams.group && this.props.row + '_left' === zone ) {
-							besideItems = this.props.items[ this.props.row + '_left_center' ];
-						}
-						if ( 'header_desktop_items' === this.props.controlParams.group && this.props.row + '_right' === zone ) {
-							besideItems = this.props.items[ this.props.row + '_right_center' ];
-						}
-						return <DropComponent removeItem={ ( remove, removeRow, removeZone ) => this.props.removeItem( remove, removeRow, removeZone ) } focusItem={ ( focus ) => this.props.focusItem( focus ) } hideDrop={ () => this.props.hideDrop() } showDrop={ () => this.props.showDrop() } onUpdate={ ( updateRow, updateZone, updateItems ) => this.props.onUpdate( updateRow, updateZone, updateItems ) } zone={ zone } row={ this.props.row } choices={ this.props.choices } key={ zone } items={ this.props.items[zone] } centerItems={ besideItems } controlParams={ this.props.controlParams } onAddItem={ ( updateRow, updateZone, updateItems ) => this.props.onAddItem( updateRow, updateZone, updateItems ) } settings={ this.props.settings } />;
-					} ) }
-				</div>
+					}
+					if ('header_desktop_items' === controlParamsGroup && props.row + '_left' === zone) {
+						besideItems = props.items[props.row + '_left_center'];
+					}
+					if ('header_desktop_items' === controlParamsGroup && props.row + '_right' === zone) {
+						besideItems = props.items[props.row + '_right_center'];
+					}
+					return <DropComponent removeItem={(remove, removeRow, removeZone) => removeItem(remove, removeRow, removeZone)} focusItem={(focus) => props.focusItem(focus)} hideDrop={() => props.hideDrop()} showDrop={() => props.showDrop()} onUpdate={(updateRow, updateZone, updateItems) => props.onUpdate(updateRow, updateZone, updateItems)} zone={zone} row={props.row} choices={props.choices} key={zone} items={props.items[zone]} centerItems={besideItems} controlParams={controlParams} onAddItem={(updateRow, updateZone, updateItems) => props.onAddItem(updateRow, updateZone, updateItems)} settings={props.settings} />;
+				})}
 			</div>
-		);
-	}
+		</div>
+	</>
 }
 export default RowComponent;
