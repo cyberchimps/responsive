03d6a886f2da148876700e24f432466098a2aa61
Saurabh Bagde <saurabh.bagde@hbwsl.com>
2021-10-29 04:15:06 +0000

    Merged in feature/RESPTHEME-172 (pull request #1572)
    Feature/RESPTHEME-172
    
    * Builder moved to customize preview
    
    * Merged in develop (pull request #1571)
    
    Develop: Updated changelog of the theme and changed version to 4.7.1 in multiple files
    
    * Develop: Updated changelog of the theme and changed version to 4.7.1 in multiple files
    
    
    Approved-by: Saurabh Bagde
    Approved-by: Ratnesh Kadam
    * Off canvas section for small screen customization
    
    * Merge branch 'feature/RESPTHEME-172' of https://bitbucket.org/cyberchimps/responsive into feature/RESPTHEME-172
    
    * Updated render callback
    
    * Merge branch 'develop' of https://bitbucket.org/cyberchimps/responsive into feature/RESPTHEME-172
    
    
    Approved-by: Ratnesh Kadam

--- core/includes/customizer/settings/class-responsive-header-footer-builder.php
+++ core/includes/customizer/settings/class-responsive-header-footer-builder.php
@@ -96,11 +96,11 @@ if ( ! class_exists( 'Responsive_Header_Footer_Builder' ) ) :
 				),
 			);
 			$wp_customize->add_setting(
-				'responsive_header_scheme',
+				'header_desktop_items',
 				array(
 					'sanitize_callback' => 'responsive_sanitize_select',
 					'transport'         => 'refresh',
-					'default'           => 'enabled',
+					'default'           => $header_desktop_items,
 				)
 			);
 			$wp_customize->add_control(
@@ -108,10 +108,8 @@ if ( ! class_exists( 'Responsive_Header_Footer_Builder' ) ) :
 					$wp_customize,
 					'header_desktop_items',
 					array(
-						// 'type' => 'responsive-builder',
 						'section'     => 'responsive_customizer_header_builder',
-						'default'     => $header_desktop_items,
-						'settings'    => 'responsive_header_scheme',
+						'settings'    => 'header_desktop_items',
 						'context'     => array(
 							array(
 								'setting' => '__device',
@@ -121,7 +119,7 @@ if ( ! class_exists( 'Responsive_Header_Footer_Builder' ) ) :
 						'partial'     => array(
 							'selector'            => '#masthead',
 							'container_inclusive' => true,
-							'render_callback'     => 'responsive\header_markup',
+							'render_callback'     => 'header_markup',
 						),
 						'choices'     => array(
 							'logo'         => array(
@@ -177,6 +175,110 @@ if ( ! class_exists( 'Responsive_Header_Footer_Builder' ) ) :
 									'bottom_center'       => esc_html__( 'Bottom - Center', 'responsive' ),
 									'bottom_right_center' => is_rtl() ? esc_html__( 'Bottom - Left Center', 'responsive' ) : esc_html__( 'Bottom - Right Center', 'responsive' ),
 									'bottom_right'        => is_rtl() ? esc_html__( 'Bottom - Left', 'responsive' ) : esc_html__( 'Bottom - Right', 'responsive' ),
+								),
+							),
+						),
+					)
+				)
+			);
+			$header_desktop_items_default = array(
+				'popup'  => array(
+					'popup_content' => array( 'mobile-navigation' ),
+				),
+				'top'    => array(
+					'top_left'   => array(),
+					'top_center' => array(),
+					'top_right'  => array(),
+				),
+				'main'   => array(
+					'main_left'   => array( 'mobile-logo' ),
+					'main_center' => array(),
+					'main_right'  => array( 'popup-toggle' ),
+				),
+				'bottom' => array(
+					'bottom_left'   => array(),
+					'bottom_center' => array(),
+					'bottom_right'  => array(),
+				),
+			);
+			$wp_customize->add_setting(
+				'header_mobile_items',
+				array(
+					'sanitize_callback' => 'responsive_sanitize_select',
+					'transport'         => 'refresh',
+					'default'           => $header_desktop_items_default,
+				)
+			);
+			$wp_customize->add_control(
+				new Responsive_Customizer_Builder_Control(
+					$wp_customize,
+					'header_mobile_items',
+					array(
+						'section'     => 'responsive_customizer_header_builder',
+						'settings'    => 'header_mobile_items',
+						'context'     => array(
+							array(
+								'setting'  => '__device',
+								'operator' => 'in',
+								'value'    => array( 'tablet', 'mobile' ),
+							),
+						),
+						'partial'     => array(
+							'selector'            => '#mobile-header',
+							'container_inclusive' => true,
+							'render_callback'     => 'mobile_header',
+						),
+						'choices'     => array(
+							'mobile-logo'       => array(
+								'name'    => esc_html__( 'Logo', 'responsive' ),
+								'section' => 'title_tagline',
+							),
+							'mobile-navigation' => array(
+								'name'    => esc_html__( 'Mobile Navigation', 'responsive' ),
+								'section' => 'responsive_header_layout',
+							),
+							'search'            => array(
+								'name'    => esc_html__( 'Search Toggle', 'responsive' ),
+								'section' => 'responsive_header_layout',
+							),
+							'mobile-button'     => array(
+								'name'    => esc_html__( 'Button', 'responsive' ),
+								'section' => 'responsive_header_layout',
+							),
+							'mobile-social'     => array(
+								'name'    => esc_html__( 'Social', 'responsive' ),
+								'section' => 'responsive_header_layout',
+							),
+							'mobile-html'       => array(
+								'name'    => esc_html__( 'HTML', 'responsive' ),
+								'section' => 'responsive_header_layout',
+							),
+							'popup-toggle'      => array(
+								'name'    => esc_html__( 'Trigger', 'responsive' ),
+								'section' => 'responsive_header_layout',
+							),
+						),
+						'input_attrs' => array(
+							'group' => 'header_mobile_items',
+							'rows'  => array( 'popup', 'top', 'main', 'bottom' ),
+							'zones' => array(
+								'popup'  => array(
+									'popup_content' => esc_html__( 'Popup Content', 'responsive' ),
+								),
+								'top'    => array(
+									'top_left'   => is_rtl() ? esc_html__( 'Top - Right', 'responsive' ) : esc_html__( 'Top - Left', 'responsive' ),
+									'top_center' => esc_html__( 'Top - Center', 'responsive' ),
+									'top_right'  => is_rtl() ? esc_html__( 'Top - Left', 'responsive' ) : esc_html__( 'Top - Right', 'responsive' ),
+								),
+								'main'   => array(
+									'main_left'   => is_rtl() ? esc_html__( 'Main - Right', 'responsive' ) : esc_html__( 'Main - Left', 'responsive' ),
+									'main_center' => esc_html__( 'Main - Center', 'responsive' ),
+									'main_right'  => is_rtl() ? esc_html__( 'Main - Left', 'responsive' ) : esc_html__( 'Main - Right', 'responsive' ),
+								),
+								'bottom' => array(
+									'bottom_left'   => is_rtl() ? esc_html__( 'Bottom - Right', 'responsive' ) : esc_html__( 'Bottom - Left', 'responsive' ),
+									'bottom_center' => esc_html__( 'Bottom - Center', 'responsive' ),
+									'bottom_right'  => is_rtl() ? esc_html__( 'Bottom - Left', 'responsive' ) : esc_html__( 'Bottom - Right', 'responsive' ),
 								),
 							),
 						),
