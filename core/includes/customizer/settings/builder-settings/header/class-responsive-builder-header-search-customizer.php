3527a10b81db5f43f353268575f84f866594dd70
Saurabh Bagde <bagdesaurabh69@gmail.com>
2021-11-25 16:38:16 +0530

    Search label visibility

--- core/includes/customizer/settings/builder-settings/header/class-responsive-builder-header-search-customizer.php
+++ core/includes/customizer/settings/builder-settings/header/class-responsive-builder-header-search-customizer.php
@@ -61,13 +61,13 @@ if ( ! class_exists( 'Responsive_Header_Search_Customizer' ) ) :
 			$header_search_label = __( 'Header Search Label', 'responsive' );
 			responsive_text_control( $wp_customize, 'header_search_label', $header_search_label, 'responsive_customizer_header_search', 20, '', null, 'sanitize_text_field', 'text' );
 
-			$header_search_label_visiblity_desktop = __( 'Header Search Label Visibility', 'responsive' );
+			$header_search_label_visiblity_desktop = __( 'Header Search Label Visibility Desktop', 'responsive' );
 			responsive_checkbox_control( $wp_customize, 'header_search_label_visiblity_desktop', $header_search_label_visiblity_desktop, 'responsive_customizer_header_search', 25, 1 );
 
-			$header_search_label_visiblity_tablet = __( 'Header Search Label Visibility', 'responsive' );
+			$header_search_label_visiblity_tablet = __( 'Header Search Label Visibility Tablet', 'responsive' );
 			responsive_checkbox_control( $wp_customize, 'header_search_label_visiblity_tablet', $header_search_label_visiblity_tablet, 'responsive_customizer_header_search', 30, 0 );
 
-			$header_search_label_visiblity_mobile = __( 'Header Search Label Visibility', 'responsive' );
+			$header_search_label_visiblity_mobile = __( 'Header Search Label Visibility Mobile', 'responsive' );
 			responsive_checkbox_control( $wp_customize, 'header_search_label_visiblity_mobile', $header_search_label_visiblity_mobile, 'responsive_customizer_header_search', 35, 0 );
 
 			// Header search Icon.
