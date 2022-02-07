5c749ad17548df6b146bbbf43808079346ea7576
ratnesh-kadam <ratnesh.kadam@hbwsl.com>
2022-02-07 10:23:10 +0000

    gitpod configurations

--- .init.sh
+++ .init.sh
@@ -5,5 +5,6 @@ wp rewrite structure '/%year%/%monthnum%/%postname%/'
 wp plugin delete hello
 wp plugin delete akismet
 wp theme activate ${REPO_NAME}
+npm install -g grunt-cli
 
 # wp plugin install plugin_slug --activate
