41d81d5d20bbb19c6d2412c0c9d165fc9af274b6
ratnesh-kadam <ratnesh.kadam@hbwsl.com>
2022-02-07 09:26:46 +0000

    gitpod configurations

--- .init.sh
+++ .init.sh
@@ -6,4 +6,8 @@ wp plugin delete hello
 wp plugin delete akismet
 wp theme activate ${REPO_NAME}
 
+cd /workspace/responsive/public_html/wp-content/themes/responsive
+npm install
+grunt
+
 # wp plugin install plugin_slug --activate
