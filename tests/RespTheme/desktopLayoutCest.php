60b7a6f0ba08b27bf22e04bcf0e85352d1822c8b
Muskaan-Sayed <muskaansayed10@gmail.com>
2022-03-03 19:12:53 +0530

    Automation TestCases for main row

--- tests/RespTheme/desktopLayoutCest.php
+++ tests/RespTheme/desktopLayoutCest.php
@@ -1,30 +1,87 @@
 <?php
-use \page\RespTheme\Customizer;
+use \page\RespTheme\Customize;
 use \page\RespTheme\LogInAndLogOut;
+use \page\RespTheme\MainRowComponent;
+use \page\RespTheme\RespThemeHelper;
+
 
 
 class desktopLayoutCest
 {
-    public function _before(RespThemeTester $I)
-    {
-    }
+   
 
     // tests
-    public function DesktopLayoutSettings(RespThemeTester $I,LogInAndLogOut $loginAndLogout, Customizer $customizer)
+    public function DesktopLayoutSettings(RespThemeTester $I,LogInAndLogOut $loginAndLogout, Customize $customize, MainRowComponent $mainrowComponent, RespThemeHelper $helper)
     {
         $I->amGoingTo('Login as Admin');
         $loginAndLogout->userLogin($I);
-        $I->click('/html/body/div[1]/div/ul[1]/li[3]/a');
+
+        $I->amGoingTo('Open customize >> Header >> Main Row >> Main Row settings');
+       // $customize->_openDefaultSettings($I, $customize->url, $customize->header);
+        //$customizer->_openLayout($I, $mainRowComponent->topRow, $mainRowComponent->desktopLayout);
+        $I->click($customize->url);
         $I->wait(2);
-        $I->scrollTo('html/body/div[1]/form/div[3]/div[2]/div[2]/ul[1]/li[37]/h3/');
+        $I->click($customize->header);
         $I->wait(2);
-        $I->click('html/body/div[1]/form/div[3]/div[2]/div[2]/ul[1]/li[37]/h3/');
+        $I->click($mainrowComponent->mainRowButton);
+        
+        $I->amGoingTo('check the  layout settings for top row');
+        $I->selectOption($mainrowComponent->desktopLayout, 'Fullwidth');
         $I->wait(2);
-        $I->click('/html/body/div[1]/form/div[3]/div[2]/div[2]/ul[6]/li[6]/h3');
+        $I->selectOption($mainrowComponent->tabletLayout, 'Fullwidth');
+        $I->wait(2);
+        $I->selectOption($mainrowComponent->mobileLayout, 'Contained');
+        $I->wait(2);
+
+        
+        $I->amGoingTo('check the min settings for main row');
+        $I->fillField($mainrowComponent->minheightDesktop, '0');
+        $I->wait(2);
+        $I->fillField($mainrowComponent->minheightTablet, '0');
+        $I->wait(2);
+        $I->fillField($mainrowComponent->minheightMobile, '0');
+        $I->wait(2);
+
+        $I->amGoingTo('check the background color settings');
+        $I->click($mainrowComponent->backgroundDesktop);
+        $I->wait(2);
+        $I->click($mainrowComponent->colorDesktop);
+        $I->wait(2);
+        $I->click($mainrowComponent->backgroundTablet);
+         $I->wait(2);
+         $I->click($mainrowComponent->colorTablet);
+         $I->wait(2);
+         $I->click($mainrowComponent->backgroundMobile);
+         $I->wait(2);
+         $I->click($mainrowComponent->colorMobile);
+         $I->wait(2);
+
+         $I->amGoingTo('check the padding settings');
+         $I->click($mainrowComponent->desktopPadding);
+         $I->fillfield($mainrowComponent->desktopPField);
+         $I->wait(2);
+         $I->click($mainrowComponent->tabletPadding);
+         $I->fillfield($mainrowComponent->tabletPField);
+         $I->wait(2);
+         $I->click($mainrowComponent->mobilePadding);
+         $I->fillfield($mainrowComponent->mobilePField);
+         $I->wait(2);
+
+        
+        $I->click($mainrowComponent->publishButton);
         $I->wait(3);
-        $I->selectOption('/html/body/div[1]/form/div[3]/div[2]/div[2]/ul[55]/li[2]/select','Contained');
+
+        $I->amGoingTo('check on the front end');
+        $I->amOnPage('/');
+       // $I->seeElement('$mianrowComponent->');
+        $I->click($customize->url);
         $I->wait(2);
-        $I->selectOption('/html/body/div[1]/form/div[3]/div[2]/div[2]/ul[55]/li[2]/select', 'Fullwidth');
+        $I->scrollTo($customize->header);
         $I->wait(2);
+        $I->click($customize->header);
+        $I->wait(2);
+        $I->click($mainrowComponent->mainRowButton);
+
     }
 }
+
