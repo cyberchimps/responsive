575a19036671a72b2c7de7503e683d58bdd505bd
Divya <divya.narhe@hbwsl.com>
2022-11-16 14:28:42 +0530

    Add Rating feature on banner & change screenshot

--- admin/class-responsive-plugin-install-helper.php
+++ admin/class-responsive-plugin-install-helper.php
@@ -60,14 +60,15 @@ class Responsive_Plugin_Install_Helper {
 	public function get_button_html( $slug, $settings = array() ) {
 		$button   = '';
 		$redirect = admin_url( 'admin.php?page=responsive-add-ons' );
-		$state = $this->check_plugin_state( $slug );
+		$reviewurl      = 'https://wordpress.org/support/plugin/responsive-add-ons/reviews/';
+		$state    = $this->check_plugin_state( $slug );
 		if ( empty( $slug ) ) {
 			return '';
 		}
 
 		$additional = '';
 
-		if ( 'deactivate' === $state ) {
+		if ( 'rateus' === $state ) {
 			$additional = ' action_button active';
 		}
 
@@ -93,24 +94,13 @@ class Responsive_Plugin_Install_Helper {
 			case 'activate':
 				$button .= '<a  data-redirect="' . esc_url( $redirect ) . '" data-slug="' . esc_attr( $slug ) . '" class="activate-now button button-primary" href="' . esc_url( $nonce ) . '" aria-label="Activate ' . esc_attr( $slug ) . '">' . esc_html__( 'Activate', 'responsive' ) . '</a>';
 				break;
-
-			case 'deactivate':
-				$nonce = add_query_arg(
-					array(
-						'action'        => 'deactivate',
-						'plugin'        => rawurlencode( $plugin_link_suffix ),
-						'plugin_status' => 'all',
-						'paged'         => '1',
-						'_wpnonce'      => wp_create_nonce( 'deactivate-plugin_' . $plugin_link_suffix ),
-					),
-					network_admin_url( 'plugins.php' )
-				);
-
-				$button .= '<a  data-redirect="' . esc_url( $redirect ) . '" data-slug="' . esc_attr( $slug ) . '" class="deactivate-now button" href="' . esc_url( $nonce ) . '" data-name="' . esc_attr( $slug ) . '" aria-label="Deactivate ' . esc_attr( $slug ) . '">' . esc_html__( 'Deactivate', 'responsive' ) . '</a>';
+			
+			case 'rateus':
+				$button .= '<a  data-redirect="' . esc_url( $redirect ) . '" data-slug="' . esc_attr( $slug ) . '" class="rateus-now button" href="' . esc_url( $reviewurl ) . '" target="_blank" data-name="' . esc_attr( $slug ) . '" aria-label="Rate Us ' . esc_attr( $slug ) . '">' . __( 'Rate  us ★★★★★' , 'responsive' ) . ' </a> ';
 				break;
 
 			case 'enable_cpt':
-				$url     = admin_url( 'admin.php?page=jetpack#/settings' );
+				$url     = admin_url( 'admin . php ? page = jetpack// /settings' );
 				$button .= '<a  data-redirect="' . esc_url( $redirect ) . '" class="button" href="' . esc_url( $url ) . '">' . esc_html__( 'Activate', 'responsive' ) . ' ' . esc_html__( 'Jetpack Portfolio', 'responsive' ) . '</a>';
 				break;
 		} // End switch.
@@ -125,18 +115,18 @@ class Responsive_Plugin_Install_Helper {
 	 *
 	 * @return bool
 	 */
-	public function get_deactivate_content( $slug ) {
+	public function get_rateus_content( $slug ) {
 		$state = $this->check_plugin_state( $slug );
 		if ( empty( $slug ) ) {
 			return '';
 		}
 
-		$deactivate = '';
+		$rateus = '';
 
-		if ( 'deactivate' === $state ) {
-			$deactivate .= '<div class="deactivate-ready-site">';
+		if ( 'ratedus' === $state ) {
+			$rateus .= '<div class="rateus-ready-site">';
 		}
-		return $deactivate;
+		return $rateus;
 	}
 
 	/**
@@ -146,18 +136,18 @@ class Responsive_Plugin_Install_Helper {
 	 *
 	 * @return bool
 	 */
-	public function get_deactivate_end_content( $slug ) {
+	public function get_rateus_end_content( $slug ) {
 		$state = $this->check_plugin_state( $slug );
 		if ( empty( $slug ) ) {
 			return '';
 		}
 
-		$deactivate_end = '';
+		$rateus_end = '';
 
-		if ( 'deactivate' === $state ) {
-			$deactivate_end .= '</div>';
+		if ( 'rateus' === $state ) {
+			$rateus_end .= '</div>';
 		}
-		return $deactivate_end;
+		return $rateus_end;
 	}
 
 	/**
@@ -171,8 +161,8 @@ class Responsive_Plugin_Install_Helper {
 		$plugin_link_suffix = self::get_plugin_path( $slug );
 
 		if ( file_exists( ABSPATH . 'wp-content/plugins/' . $plugin_link_suffix ) ) {
-			$needs = is_plugin_active( $plugin_link_suffix ) ? 'deactivate' : 'activate';
-			if ( 'deactivate' === $needs && ! post_type_exists( 'portfolio' ) && 'jetpack' === $slug ) {
+			$needs = is_plugin_active( $plugin_link_suffix ) ? 'rateus' : 'activate';
+			if ( 'rateus' === $needs && ! post_type_exists( 'portfolio' ) && 'jetpack' === $slug ) {
 				return 'enable_cpt';
 			}
 
