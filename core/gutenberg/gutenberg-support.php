fe880e7108444a59bb0ecbe1115b32e5af7a27ee
Maheswar M <maheswar.manyam@hbwsl.com>
2025-01-08 15:43:53 +0530

    Make form fields settings compatible with gutenberg editor

--- core/gutenberg/gutenberg-support.php
+++ core/gutenberg/gutenberg-support.php
@@ -147,7 +147,101 @@ function responsive_gutenberg_customizer_css() {
 	$button_typography                 = get_theme_mod( 'button_typography');
 	$button_tablet_typography          = get_theme_mod( 'button_tablet_typography' );
 	$button_mobile_typography          = get_theme_mod( 'button_mobile_typography' );
+
+	$inputs_border_color               = esc_html( get_theme_mod( 'responsive_inputs_border_color', Responsive\Core\get_responsive_customizer_defaults( 'inputs_border' ) ) );
+	$inputs_text_color                 = esc_html( get_theme_mod( 'responsive_inputs_text_color', Responsive\Core\get_responsive_customizer_defaults( 'inputs_text' ) ) );
+	$inputs_background_color           = esc_html( get_theme_mod( 'responsive_inputs_background_color', Responsive\Core\get_responsive_customizer_defaults( 'inputs_background' ) ) );
+
+	$label_color                       = esc_html( get_theme_mod( 'responsive_label_color', Responsive\Core\get_responsive_customizer_defaults( 'label' ) ) );
+	// Input fields padding.
+	$inputs_padding_right              = esc_html( get_theme_mod( 'responsive_inputs_right_padding', 3 ) );
+	$inputs_padding_left               = esc_html( get_theme_mod( 'responsive_inputs_left_padding', 3 ) );
+	$inputs_padding_top                = esc_html( get_theme_mod( 'responsive_inputs_top_padding', 3 ) );
+	$inputs_padding_bottom             = esc_html( get_theme_mod( 'responsive_inputs_bottom_padding', 3 ) );
+
+	$inputs_tablet_padding_right       = esc_html( get_theme_mod( 'responsive_inputs_tablet_right_padding', 3 ) );
+	$inputs_tablet_padding_left        = esc_html( get_theme_mod( 'responsive_inputs_tablet_left_padding', 3 ) );
+	$inputs_tablet_padding_top         = esc_html( get_theme_mod( 'responsive_inputs_tablet_top_padding', 3 ) );
+	$inputs_tablet_padding_bottom      = esc_html( get_theme_mod( 'responsive_inputs_tablet_bottom_padding', 3 ) );
+
+	$inputs_mobile_padding_right       = esc_html( get_theme_mod( 'responsive_inputs_mobile_right_padding', 3 ) );
+	$inputs_mobile_padding_left        = esc_html( get_theme_mod( 'responsive_inputs_mobile_left_padding', 3 ) );
+	$inputs_mobile_padding_top         = esc_html( get_theme_mod( 'responsive_inputs_mobile_top_padding', 3 ) );
+	$inputs_mobile_padding_bottom      = esc_html( get_theme_mod( 'responsive_inputs_mobile_bottom_padding', 3 ) );
+
+	// Input fields border radius.
+	$inputs_top_left_radius            = esc_html( get_theme_mod( 'responsive_inputs_radius_top_left_radius', 0 ) );
+	$inputs_top_right_radius           = esc_html( get_theme_mod( 'responsive_inputs_radius_top_right_radius', 0 ) );
+	$inputs_bottom_right_radius        = esc_html( get_theme_mod( 'responsive_inputs_radius_bottom_right_radius', 0 ) );
+	$inputs_bottom_left_radius         = esc_html( get_theme_mod( 'responsive_inputs_radius_bottom_left_radius', 0 ) );
+
+	$inputs_tablet_top_left_radius     = esc_html( get_theme_mod( 'responsive_inputs_radius_tablet_top_left_radius', 0 ) );
+	$inputs_tablet_top_right_radius    = esc_html( get_theme_mod( 'responsive_inputs_radius_tablet_top_right_radius', 0 ) );
+	$inputs_tablet_bottom_right_radius = esc_html( get_theme_mod( 'responsive_inputs_radius_tablet_bottom_right_radius', 0 ) );
+	$inputs_tablet_bottom_left_radius  = esc_html( get_theme_mod( 'responsive_inputs_radius_tablet_bottom_left_radius', 0 ) );
+
+	$inputs_mobile_top_left_radius     = esc_html( get_theme_mod( 'responsive_inputs_radius_mobile_top_left_radius', 0 ) );
+	$inputs_mobile_top_right_radius    = esc_html( get_theme_mod( 'responsive_inputs_radius_mobile_top_right_radius', 0 ) );
+	$inputs_mobile_bottom_right_radius = esc_html( get_theme_mod( 'responsive_inputs_radius_mobile_bottom_right_radius', 0 ) );
+	$inputs_mobile_bottom_left_radius  = esc_html( get_theme_mod( 'responsive_inputs_radius_mobile_bottom_left_radius', 0 ) );
+
+	// Inputs Border width.
+	$input_border_top_width            = esc_html( get_theme_mod( 'responsive_inputs_border_width_top_border', 1 ) );
+	$input_border_right_width          = esc_html( get_theme_mod( 'responsive_inputs_border_width_right_border', 1 ) );
+	$input_border_bottom_width         = esc_html( get_theme_mod( 'responsive_inputs_border_width_bottom_border', 1 ) );
+	$input_border_left_width           = esc_html( get_theme_mod( 'responsive_inputs_border_width_left_border', 1 ) );
+
+	$input_border_tablet_top_width     = esc_html( get_theme_mod( 'responsive_inputs_border_width_tablet_top_border', 1 ) );
+	$input_border_tablet_right_width   = esc_html( get_theme_mod( 'responsive_inputs_border_width_tablet_right_border', 1 ) );
+	$input_border_tablet_bottom_width  = esc_html( get_theme_mod( 'responsive_inputs_border_width_tablet_bottom_border', 1 ) );
+	$input_border_tablet_left_width    = esc_html( get_theme_mod( 'responsive_inputs_border_width_tablet_left_border', 1 ) );
+
+	$input_border_mobile_top_width     = esc_html( get_theme_mod( 'responsive_inputs_border_width_mobile_top_border', 1 ) );
+	$input_border_mobile_right_width   = esc_html( get_theme_mod( 'responsive_inputs_border_width_mobile_right_border', 1 ) );
+	$input_border_mobile_bottom_width  = esc_html( get_theme_mod( 'responsive_inputs_border_width_mobile_bottom_border', 1 ) );
+	$input_border_mobile_left_width    = esc_html( get_theme_mod( 'responsive_inputs_border_width_mobile_left_border', 1 ) );
+	// Form Fields -> Label Color.
+	$label_color                       = esc_html( get_theme_mod( 'responsive_label_color', Responsive\Core\get_responsive_customizer_defaults( 'label' ) ) );
+	$inputs_background_image           = get_theme_mod( 'responsive_inputs_background_image_toggle' ) ? esc_url( get_theme_mod( 'responsive_inputs_background_image' ) ) : null ;
+	$input_typography                  = get_theme_mod( 'input_typography');
+	$input_tablet_typography           = get_theme_mod( 'input_tablet_typography' );
+	$input_mobile_typography           = get_theme_mod( 'input_mobile_typography' );
+
 	$custom_css = '';
+
+	$block_editor_form_fields_css_selector = '
+	.editor-styles-wrapper select,
+	.editor-styles-wrapper textarea:not(.block-editor-plain-text),
+	.editor-styles-wrapper input[type=tel],
+	.editor-styles-wrapper input[type=email],
+	.editor-styles-wrapper input[type=number],
+	.editor-styles-wrapper input[type=search],
+	.editor-styles-wrapper input[type=text],
+	.editor-styles-wrapper input[type=date],
+	.editor-styles-wrapper input[type=datetime],
+	.editor-styles-wrapper input[type=datetime-local],
+	.editor-styles-wrapper input[type=month],
+	.editor-styles-wrapper input[type=password],
+	.editor-styles-wrapper input[type=range],
+	.editor-styles-wrapper input[type=time],
+	.editor-styles-wrapper input[type=url],
+	.editor-styles-wrapper input[type=week],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=date],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=datetime],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=datetime-local],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=email],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=month],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=number],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=password],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=range],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=search],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=tel],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=text],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=time],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=url],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form input[type=week],
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form select,
+	.editor-styles-wrapper div.wpforms-container-full .wpforms-form textarea';
 
 	if ( $h1_typography ) {
 		$custom_css .= '
@@ -305,6 +399,25 @@ function responsive_gutenberg_customizer_css() {
 		background-color:' . $button_hover_color . ';
 	}
 
+	' . $block_editor_form_fields_css_selector . ' {
+		color: ' . $inputs_text_color . ';
+		background-color: ' . $inputs_background_color . ';
+		border-color: ' . $inputs_border_color. ';
+		border-top-width: '. $input_border_top_width. 'px;
+		border-right-width: '. $input_border_right_width. 'px;
+		border-bottom-width: '. $input_border_bottom_width. 'px;
+		border-left-width: '. $input_border_left_width. 'px;
+		border-radius: ' . responsive_spacing_css( $inputs_top_left_radius, $inputs_top_right_radius, $inputs_bottom_right_radius, $inputs_bottom_left_radius ) . ';
+		line-height: 1.75;
+		padding: ' . responsive_spacing_css( $inputs_padding_top, $inputs_padding_right, $inputs_padding_bottom, $inputs_padding_left ) . ';
+		height: auto;
+	}
+
+	.editor-styles-wrapper label,
+	.editor-styles-wrapper .wp-block-search__label {
+		color:' . $label_color .';
+	}
+
 	@media screen and ( max-width: 992px ) {
 		.editor-styles-wrapper .wp-block-button__link,
 		.editor-styles-wrapper .wp-block-file__button,
@@ -312,6 +425,14 @@ function responsive_gutenberg_customizer_css() {
 		.editor-styles-wrapper button{
 			padding: ' . responsive_spacing_css( $buttons_tablet_padding_top, $buttons_tablet_padding_right, $buttons_tablet_padding_bottom, $buttons_tablet_padding_left ) . ';
 			border-radius:' . responsive_spacing_css( $button_tablet_top_left_radius, $button_tablet_top_right_radius, $button_tablet_bottom_right_radius, $button_tablet_bottom_left_radius ) . ';
+		}
+		' . $block_editor_form_fields_css_selector . ' {
+			padding: ' . responsive_spacing_css( $inputs_tablet_padding_top, $inputs_tablet_padding_right, $inputs_tablet_padding_bottom, $inputs_tablet_padding_left ) . ';
+			border-radius: ' . responsive_spacing_css( $inputs_tablet_top_left_radius, $inputs_tablet_top_right_radius, $inputs_tablet_bottom_right_radius, $inputs_tablet_bottom_left_radius ) . ';
+			border-top-width: '. $input_border_tablet_top_width. 'px;
+			border-right-width: '. $input_border_tablet_right_width. 'px;
+			border-bottom-width: '. $input_border_tablet_bottom_width. 'px;
+			border-left-width: '. $input_border_tablet_left_width. 'px;
 		}
 	}
 	@media screen and ( max-width: 576px ) {
@@ -321,6 +442,14 @@ function responsive_gutenberg_customizer_css() {
 		.editor-styles-wrapper button{
 		    padding: ' . responsive_spacing_css( $buttons_mobile_padding_top, $buttons_mobile_padding_right, $buttons_mobile_padding_bottom, $buttons_mobile_padding_left ) . ';
 			border-radius:' . responsive_spacing_css( $button_mobile_top_left_radius, $button_mobile_top_right_radius, $button_mobile_bottom_right_radius, $button_mobile_bottom_left_radius ) . ';
+		}
+		' . $block_editor_form_fields_css_selector . ' {
+			padding: ' . responsive_spacing_css( $inputs_mobile_padding_top, $inputs_mobile_padding_right, $inputs_mobile_padding_bottom, $inputs_mobile_padding_left ) . ';
+			border-radius: ' . responsive_spacing_css( $inputs_mobile_top_left_radius, $inputs_mobile_top_right_radius, $inputs_mobile_bottom_right_radius, $inputs_mobile_bottom_left_radius ) . ';
+			border-top-width: '. $input_border_mobile_top_width. 'px;
+			border-right-width: '. $input_border_mobile_right_width. 'px;
+			border-bottom-width: '. $input_border_mobile_bottom_width. 'px;
+			border-left-width: '. $input_border_mobile_left_width. 'px;
 		}
 	}
 	';
@@ -336,6 +465,16 @@ function responsive_gutenberg_customizer_css() {
 			background-size: cover;
 			background-attachment: scroll;
 		}";
+	}
+	if ( $inputs_background_image ) {
+		$custom_css .= "
+			$block_editor_form_fields_css_selector {
+				background-color: {$inputs_background_color};
+				background-image: linear-gradient(to right, {$inputs_background_color}, {$inputs_background_color}), url({$inputs_background_image});
+				background-repeat: no-repeat;
+				background-size: cover;
+				background-attachment: scroll;
+			}";
 	}
 	if ( $button_typography ) {
 		$custom_css .= "
@@ -379,6 +518,40 @@ function responsive_gutenberg_customizer_css() {
 		}
 		$custom_css .= "}}";
 	}
+	// Apply Form Fields Typography styles.	
+	if ( $input_typography ) {
+		$custom_css .= "
+			$block_editor_form_fields_css_selector {
+		";
+		foreach ( $input_typography as $key => $value ) {
+			if ( 'letter-spacing' === $key ) {
+				$custom_css .= $key . ':' . $value . 'px;';
+			} else {
+				$custom_css .= $key . ':' . $value . ';';
+			}
+		}
+		$custom_css .= "}";
+	}
+	if ( $input_tablet_typography ) {
+		$custom_css .= "
+		@media screen and ( max-width: 992px ) {
+			$block_editor_form_fields_css_selector {
+		";
+		foreach ( $input_tablet_typography as $key => $value ) {
+			$custom_css .= $key . ':' . $value . ';';
+		}
+		$custom_css .= "}}";
+	}
+	if ( $input_mobile_typography ) {
+		$custom_css .= "
+		@media screen and ( max-width: 576px ) {
+			$block_editor_form_fields_css_selector {
+		";
+		foreach ( $input_mobile_typography as $key => $value ) {
+			$custom_css .= $key . ':' . $value . ';';
+		}
+		$custom_css .= "}}";
+	}
 	
 
 	return $custom_css;
@@ -393,7 +566,7 @@ function responsive_block_styles() {
 	}
 	wp_enqueue_style( 'responsive-gutenberg-blocks', get_stylesheet_directory_uri() . '/core/css/gutenberg-editor' . $suffix . '.css', array(), '4.1' );
 	wp_enqueue_script( 'responsive-block-editor-scripts', get_stylesheet_directory_uri() . '/core/js/block-editor-script.js', array(), RESPONSIVE_THEME_VERSION, false );
-	wp_add_inline_style( 'responsive-gutenberg-blocks', apply_filters( 'responsive_gutenberg_editor_css', responsive_gutenberg_customizer_css() ) );
+	wp_add_inline_style( 'responsive-gutenberg-blocks', apply_filters( 'responsive_gutenberg_editor_css', responsive_minimize_css( responsive_gutenberg_customizer_css() ) ) );
 
 	// Add customizer colors to Gutenberg editor in backend.
 	wp_add_inline_style( 'responsive-gutenberg-blocks', responsive_gutenberg_colors( responsive_gutenberg_color_palette() ) );
