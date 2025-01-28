199d747315be00a3ec0f3730ef398593a9051a46
Shubham Rawat <shubham.rawat@hbwsl.com>
2025-01-28 12:10:55 +0530

    Fix the preview issue of font presets
    The previous font was not being applied to the preview when font presets reset button was used to reset the font.
    The fonts were not coming correctly in the frontend when the woocommerce plugin was active.

--- core/includes/customizer/assets/js/customize-preview-font-presets.js
+++ core/includes/customizer/assets/js/customize-preview-font-presets.js
@@ -70,6 +70,38 @@
                 control.set('');
                 jQuery( 'style.customizer-typography-font-preset-body-font-family' ).remove();
                 jQuery( 'style.customizer-typography-font-preset-headings-font-family' ).remove();
+
+                 // Trigger body_typography[font-family] with its current value
+                var resetValues = ["body_typography[font-family]",
+                    "body_typography[font-weight]",
+                    "headings_typography[font-family]",
+                    "headings_typography[font-weight]",
+                    "heading_h1_typography[font-family]",
+                    "heading_h1_typography[font-weight]",
+                    "heading_h2_typography[font-family]",
+                    "heading_h2_typography[font-weight]",
+                    "heading_h3_typography[font-family]",
+                    "heading_h3_typography[font-weight]",
+                    "heading_h4_typography[font-family]",
+                    "heading_h4_typography[font-weight]",
+                    "heading_h5_typography[font-family]",
+                    "heading_h5_typography[font-weight]",
+                    "heading_h6_typography[font-family]",
+                    "heading_h6_typography[font-weight]",
+                    "meta_typography[font-family]",
+                    "meta_typography[font-weight]",
+                ];
+
+                resetValues.forEach(function (value) {
+                    var control = api(value);
+                    if (control) {
+                        var currentValue = control.get(); // Get the current value
+                        //if directly set to currentValue it will not trigger as it checks for change in value
+                        control.set("temp-value"); // Re-set to temp value so that it get triggered
+                        control.set(currentValue); // Re-set to trigger the bind logic
+                    }
+                });
+
                 return ;
             }
             var bodyFont = choices[value].bodyFont;
@@ -103,7 +135,7 @@
         jQuery( 'style.customizer-typography-font-preset-body-font-family' ).remove();
         jQuery( 'head' ).append(
             '<style class="customizer-typography-font-preset-body-font-family">'
-            + 'body' + '{ font-family:' + fontFamily +'; font-weight:' + fontWeight + ';}'
+            + 'body, .post-meta *' + '{ font-family:' + fontFamily +'; font-weight:' + fontWeight + ';}'
             + '</style>'
         );
     }
