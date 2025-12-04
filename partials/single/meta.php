d17a8e21cfa30e61d8ae61b588cf44a9b1ec0170
DAnn2012 <dannunziata@hotmail.it>
2025-12-04 10:01:15 +0100

    Update meta.php

--- partials/single/meta.php
+++ partials/single/meta.php
@@ -60,7 +60,7 @@ do_action( 'responsive_before_single_post_meta' );
 					<?php
 					printf(
 						/* translators: 1: class, 2: date */
-						'<i class="icon-calendar" aria-hidden="true"></i><span>' . esc_html_e( 'Posted on ', 'responsive' ) . '</span><span class="%1$s" itemprop="datePublished">%2$s</span>',
+						'<i class="icon-calendar" aria-hidden="true"></i><span>' . esc_html__( 'Posted on ', 'responsive' ) . '</span><span class="%1$s" itemprop="datePublished">%2$s</span>',
 						'meta-prep meta-prep-author posted',
 						sprintf(
 							'<a href="%1$s" aria-label="%2$s" title="%2$s" rel="bookmark"><time class="timestamp updated" datetime="%3$s" itemprop="dateModified">%4$s</time></a>',
@@ -81,7 +81,7 @@ do_action( 'responsive_before_single_post_meta' );
 					<?php
 						printf(
 							/* translators: 1: class, 2: date */
-							'<i class="icon-calendar" aria-hidden="true"></i><span>' . esc_html_e( 'Last updated on ', 'responsive' ) . '</span><span class="%1$s" itemprop="datePublished">%2$s</span>',
+							'<i class="icon-calendar" aria-hidden="true"></i><span>' . esc_html__( 'Last updated on ', 'responsive' ) . '</span><span class="%1$s" itemprop="datePublished">%2$s</span>',
 							'meta-prep meta-prep-author posted',
 							sprintf(
 								'<a href="%1$s" aria-label="%2$s" title="%2$s" rel="bookmark"><time class="timestamp updated" datetime="%3$s" itemprop="dateModified">%4$s</time></a>',
