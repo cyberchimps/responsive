1881965bdbaaede62d2cc6f5737e163e15ed4e62
Tejas Sonawane <tejas.sonawane@hbwsl.com>
2026-03-31 18:12:44 +0530

    Implement enable disable comments

--- core/includes/customizer/assets/js/customize-preview-color-control.js
+++ core/includes/customizer/assets/js/customize-preview-color-control.js
@@ -4919,4 +4919,11 @@
         });
     });
 
+    // Comments Border Color.
+    api('responsive_comments_border_color_color', function(value) {
+        value.bind(function(newval) {
+            $('body .site .comments-area').css('border-color', newval);
+        });
+    });
+
 } )( jQuery );
