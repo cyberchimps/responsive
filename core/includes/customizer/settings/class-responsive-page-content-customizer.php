2337889221e57d14f04fd2ed55e295a4096a2cda
Prasada <prasada.pujari@hbwsl.com>
2026-07-31 11:10:56 +0530

    added minor changes Show Comments toggle-Single Page

--- core/includes/customizer/settings/class-responsive-page-content-customizer.php
+++ core/includes/customizer/settings/class-responsive-page-content-customizer.php
@@ -91,6 +91,8 @@ if ( ! class_exists( 'Responsive_Page_Content_Customizer' ) ) :
 				$general_tab_ids_prefix . 'container_style_separator',
 				$general_tab_ids_prefix . 'content_top_bottom_spacing',
 				$general_tab_ids_prefix . 'content_alignment_separator',
+				$general_tab_ids_prefix . 'show_comments',
+				$general_tab_ids_prefix . 'content_vertical_separator',
 
 			);
 			responsive_tabs_button_control( $wp_customize, 'page_tabs', $tabs_label, 'responsive_page', 10, '', 'responsive_page_content_general_tab', 'responsive_page_content_design_tab', $general_tab_ids, $design_tab_ids, null );
