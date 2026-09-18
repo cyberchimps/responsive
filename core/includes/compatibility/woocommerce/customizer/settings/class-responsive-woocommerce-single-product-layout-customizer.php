2b4a42a13fffa67d2b735a42964d756c5bc85463
Arghadeep Dey <arghadeep.dey@hbwsl.com>
2026-09-18 17:14:41 +0530

    fixing priority issue

--- core/includes/compatibility/woocommerce/customizer/settings/class-responsive-woocommerce-single-product-layout-customizer.php
+++ core/includes/compatibility/woocommerce/customizer/settings/class-responsive-woocommerce-single-product-layout-customizer.php
@@ -192,7 +192,7 @@ if ( ! class_exists( 'Responsive_Woocommerce_Single_Product_Layout_Customizer' )
 
 			// Product Image Width.
 			$single_product_image_width_label = esc_html__( 'Image Width (%)', 'responsive' );
-			responsive_drag_number_control( $wp_customize, 'single_product_image_width', $single_product_image_width_label, 'responsive_woocommerce_single_product_layout', 71, 48, null, 70, 20, 'refresh' );
+			responsive_drag_number_control( $wp_customize, 'single_product_image_width', $single_product_image_width_label, 'responsive_woocommerce_single_product_layout', 46, 48, null, 70, 20, 'refresh' );
 
 			/*
 			 * Color settings for floating bar
